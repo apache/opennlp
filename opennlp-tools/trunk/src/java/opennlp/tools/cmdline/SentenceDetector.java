936cc32a7361e55f6939e0facc7f576f80741593
joernkottmann <joernkottmann@unknown>
2008-11-05 14:26:08 +0000

    Initial version of the SentenceDetector command line util
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048183 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/java/opennlp/tools/cmdline/SentenceDetector.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreemnets.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0 
+ * (the "License"); you may not use this file except in compliance with 
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.cmdline;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.util.InvalidFormatException;
+
+/**
+ * A sentence detector which uses a maxent model to predict the sentences.
+ */
+public class SentenceDetector{
+  
+  /**
+   * Perform sentence detection the input stream.
+   * 
+   * A newline will be treated as a paragraph boundary.
+   * 
+   * <p>java opennlp.tools.lang.SentenceDetector model < "First sentence. Second sentence? Here is another one. And so on and so forth - you get the idea."
+   */
+  public static void main(String[] args) throws IOException, InvalidFormatException {
+    
+    if (args.length != 1) {
+      System.err.print("Usage java opennlp.tools.cmdline.SentenceDetector model < text");
+      System.exit(1);
+    }
+    
+    SentenceModel model = SentenceModel.create(new FileInputStream(args[0]));
+    
+    SentenceDetectorME sdetector = new SentenceDetectorME(model);
+    
+    StringBuilder para = new StringBuilder();
+    
+    BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+    for (String line = inReader.readLine(); line != null; line = inReader.readLine()) {
+      if (line.equals("")) {
+        if (para.length() != 0) {
+          String[] sents = sdetector.sentDetect(para.toString());
+          for (int si = 0, sn = sents.length; si < sn; si++) {
+            System.out.println(sents[si]);
+          }
+        }
+        System.out.println();
+        para.setLength(0);
+      }
+      else {
+        para.append(line).append(" ");
+      }
+    }
+    
+    if (para.length() != 0) {
+      String[] sents = sdetector.sentDetect(para.toString());
+      for (int si = 0, sn = sents.length; si < sn; si++) {
+        System.out.println(sents[si]);
+      }
+    }
+  }
+}
