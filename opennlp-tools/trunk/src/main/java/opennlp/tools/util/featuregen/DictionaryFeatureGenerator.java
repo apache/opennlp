68574192160998b2290e248c5194024d9077e533
Tom Morton <tsmorton@apache.org>
2009-03-09 03:12:26 +0000

    Re-structureing to make this a wrapper rather than extend the InSpanGenerator.  Also allow dictionary to be set after construction and for prefix to be omitted.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048265 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/trunk/src/main/java/opennlp/tools/util/featuregen/DictionaryFeatureGenerator.java
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/util/featuregen/DictionaryFeatureGenerator.java
@@ -18,7 +18,10 @@
 
 package opennlp.tools.util.featuregen;
 
+import java.util.List;
+
 import opennlp.tools.dictionary.Dictionary;
+import opennlp.tools.util.featuregen.FeatureGeneratorAdapter;
 import opennlp.tools.namefind.DictionaryNameFinder;
 
 /**
@@ -29,9 +32,27 @@ import opennlp.tools.namefind.DictionaryNameFinder;
  * @see DictionaryNameFinder
  * @see InSpanGenerator
  */
-public class DictionaryFeatureGenerator extends InSpanGenerator {
+public class DictionaryFeatureGenerator extends FeatureGeneratorAdapter {
 
-  public DictionaryFeatureGenerator(String prefix, Dictionary dict) {
-    super(prefix, new DictionaryNameFinder(dict));
+  private InSpanGenerator isg;
+  
+  public DictionaryFeatureGenerator(Dictionary dict) {
+    this("",dict);
   }
+  public DictionaryFeatureGenerator(String prefix, Dictionary dict) {
+    setDictionary(prefix,dict);
+  }
+  
+  public void setDictionary(Dictionary dict) {
+    setDictionary("",dict);
+  }
+  
+  public void setDictionary(String name, Dictionary dict) {
+    isg = new InSpanGenerator(name, new DictionaryNameFinder(dict));
+  }
+  
+  public void createFeatures(List<String> features, String[] tokens, int index, String[] previousOutcomes) {
+    isg.createFeatures(features, tokens, index, previousOutcomes);
+  }
+  
 }
