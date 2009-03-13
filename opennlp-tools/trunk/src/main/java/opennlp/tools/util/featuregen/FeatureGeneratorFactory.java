9960ae396802ad0385f77578fbf6d30614ca5123
joernkottmann <joernkottmann@unknown>
2009-03-13 20:49:50 +0000

    TokenNameFinder now uses a .class to define feature generation.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048278 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/util/featuregen/FeatureGeneratorFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreemnets.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package opennlp.tools.util.featuregen;
+
+/**
+ * The {@link FeatureGeneratorFactory} interface is factory for {@link AdaptiveFeatureGenerator}s.
+ * 
+ * Note:
+ * All implementing classes must be thread safe.
+ * 
+ * @see AdaptiveFeatureGenerator
+ * @see FeatureGeneratorResourceProvider
+ */
+public interface FeatureGeneratorFactory {
+  
+  /**
+   * Constructs a new {@link AdaptiveFeatureGenerator}.
+   * 
+   * Note:
+   * It is assumed that all resource objects are thread safe and can be shared
+   * between multiple instances of feature generators. If that is not the
+   * case the implementor should make a copy of the resource object.
+   * All resource objects that are included in OpenNLP can be assumed to be thread safe.
+   * 
+   * @param resourceProvider provides access to resources which are needed for feature generation.
+   * 
+   * @return the newly created feature generator
+   */
+  AdaptiveFeatureGenerator createFeatureGenerator(FeatureGeneratorResourceProvider resourceProvider);
+}
