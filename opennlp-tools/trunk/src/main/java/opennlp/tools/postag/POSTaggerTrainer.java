ba265c409c0427c874a480555383d7d7239fc3ba
Tom Morton <tsmorton@apache.org>
2009-03-15 03:36:45 +0000

    added support for sequence-based training.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048285 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/trunk/src/main/java/opennlp/tools/postag/POSTaggerTrainer.java
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/postag/POSTaggerTrainer.java
@@ -17,8 +17,10 @@
 
 package opennlp.tools.postag;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -30,7 +32,9 @@ import opennlp.maxent.PlainTextByLineDataStream;
 import opennlp.maxent.io.SuffixSensitiveGISModelWriter;
 import opennlp.model.AbstractModel;
 import opennlp.model.EventStream;
+import opennlp.model.SequenceStream;
 import opennlp.model.TwoPassDataIndexer;
+import opennlp.perceptron.SimplePerceptronSequenceTrainer;
 import opennlp.perceptron.SuffixSensitivePerceptronModelWriter;
 import opennlp.tools.dictionary.Dictionary;
 import opennlp.tools.ngram.NGramModel;
@@ -98,10 +102,27 @@ public class POSTaggerTrainer {
     return opennlp.maxent.GIS.trainModel(iterations, new TwoPassDataIndexer(es, cut));
   }
 
-  public static AbstractModel trainPerceptronModel(EventStream es, int iterations, int cut) throws IOException {
-    return new opennlp.perceptron.PerceptronTrainer().trainModel(iterations, new TwoPassDataIndexer(es, cut), cut);
+  public static AbstractModel trainPerceptronModel(EventStream es, int iterations, int cut, boolean useAverage) throws IOException {
+    return new opennlp.perceptron.PerceptronTrainer().trainModel(iterations, new TwoPassDataIndexer(es, cut, false), cut, useAverage);
   }
 
+  public static AbstractModel trainPerceptronModel(EventStream es, int iterations, int cut) throws IOException {
+    return trainPerceptronModel(es,iterations,cut,true);
+  }
+  
+  public static AbstractModel trainPerceptronSequenceModel(SequenceStream ss, int iterations, int cut, boolean useAverage) {
+    return new SimplePerceptronSequenceTrainer().trainModel(iterations, ss, cut,useAverage);
+  }
+
+  public static void test(AbstractModel model) throws IOException {
+    POSTaggerME tagger = new POSTaggerME(model, (TagDictionary) null);
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    for (String line = in.readLine(); line != null; line = in.readLine()) {
+      System.out.println(tagger.tag(line));
+    }
+  }
 
   public static void main(String[] args) {
     if (args.length == 0){
@@ -112,6 +133,7 @@ public class POSTaggerTrainer {
       String encoding = null;
       String dict = null;
       boolean perceptron = false;
+      boolean sequence = false;
       while (args[ai].startsWith("-")) {
         if (args[ai].equals("-encoding")) {
           ai++;
@@ -130,6 +152,10 @@ public class POSTaggerTrainer {
           else {
             usage();
           }
+        }
+        else if (args[ai].equals("-sequence")) {
+          ai++;
+          sequence = true;
         }
         else if (args[ai].equals("-model")) {
           ai++;
@@ -164,74 +190,112 @@ public class POSTaggerTrainer {
       }
       AbstractModel mod;
       if (dict != null) {
-        System.err.println("Building dictionary");
-
-        NGramModel ngramModel = new NGramModel();
-
-        DataStream data = new opennlp.maxent.PlainTextByLineDataStream(new java.io.FileReader(inFile));
-        while(data.hasNext()) {
-          String tagStr = (String) data.nextToken();
-          String[] tt = tagStr.split(" ");
-          String[] words = new String[tt.length];
-          for (int wi=0;wi<words.length;wi++) {
-            words[wi] =
-                tt[wi].substring(0,tt[wi].lastIndexOf('_'));
+        buildDictionary(dict, inFile, cutoff);
+      }
+      if (sequence) {
+        POSSampleSequenceStream ss;
+        if (encoding == null) {
+          if (dict == null) {
+            ss = new POSSampleSequenceStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile)))));
           }
+          else {
+            POSContextGenerator cg = new DefaultPOSContextGenerator(new Dictionary(new FileInputStream(dict)));
 
-          ngramModel.add(new StringList(words), 1, 1);
-        }
-
-        System.out.println("Saving the dictionary");
-
-        ngramModel.cutoff(cutoff, Integer.MAX_VALUE);
-        Dictionary dictionary = ngramModel.toDictionary(true);
-
-        dictionary.serialize(new FileOutputStream(dict));
-      }
-      POSSampleEventStream es;
-      if (encoding == null) {
-        if (dict == null) {
-          es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
-              new InputStreamReader(new FileInputStream(inFile)))));
+            ss = new POSSampleSequenceStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile)))),
+                cg);
+          }
         }
         else {
-          POSContextGenerator cg = new DefaultPOSContextGenerator(new Dictionary(new FileInputStream(dict)));
+          if (dict == null) {
 
-          es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
-              new InputStreamReader(new FileInputStream(inFile)))),
-              cg);
-        }
-      }
-      else {
-        if (dict == null) {
+            ss = new POSSampleSequenceStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile), encoding))));
+          }
+          else {
+            POSContextGenerator cg = new DefaultPOSContextGenerator(new Dictionary(new FileInputStream(dict)));
 
-          es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
-              new InputStreamReader(new FileInputStream(inFile), encoding))));
+            ss = new POSSampleSequenceStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile), encoding))), cg);
+          }
         }
-        else {
-          POSContextGenerator cg = new DefaultPOSContextGenerator(new Dictionary(new FileInputStream(dict)));
-
-          es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
-              new InputStreamReader(new FileInputStream(inFile), encoding))), cg);
-        }
-      }
-      if (perceptron) {
-        mod = trainPerceptronModel(es,iterations, cutoff);
+        mod = new SimplePerceptronSequenceTrainer().trainModel(iterations, ss, cutoff, true);
         System.out.println("Saving the model as: " + outFile);
         new SuffixSensitivePerceptronModelWriter(mod, outFile).persist();
       }
       else {
-        mod = trainMaxentModel(es, iterations, cutoff);
+        POSSampleEventStream es;
+        if (encoding == null) {
+          if (dict == null) {
+            es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile)))));
+          }
+          else {
+            POSContextGenerator cg = new DefaultPOSContextGenerator(new Dictionary(new FileInputStream(dict)));
 
-        System.out.println("Saving the model as: " + outFile);
+            es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile)))),
+                cg);
+          }
+        }
+        else {
+          if (dict == null) {
 
-        new SuffixSensitiveGISModelWriter(mod, outFile).persist();
+            es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile), encoding))));
+          }
+          else {
+            POSContextGenerator cg = new DefaultPOSContextGenerator(new Dictionary(new FileInputStream(dict)));
+
+            es = new POSSampleEventStream(new WordTagSampleStream(new PlainTextByLineDataStream(
+                new InputStreamReader(new FileInputStream(inFile), encoding))), cg);
+          }
+        }
+        if (perceptron) {
+          mod = trainPerceptronModel(es,iterations, cutoff);
+          System.out.println("Saving the model as: " + outFile);
+          new SuffixSensitivePerceptronModelWriter(mod, outFile).persist();
+        }
+        else {
+          mod = trainMaxentModel(es, iterations, cutoff);
+
+          System.out.println("Saving the model as: " + outFile);
+
+          new SuffixSensitiveGISModelWriter(mod, outFile).persist();
+        }
       }
-
     }
     catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private static void buildDictionary(String dict, File inFile, int cutoff)
+      throws FileNotFoundException, IOException {
+    System.err.println("Building dictionary");
+
+    NGramModel ngramModel = new NGramModel();
+
+    DataStream data = new opennlp.maxent.PlainTextByLineDataStream(new java.io.FileReader(inFile));
+    while(data.hasNext()) {
+      String tagStr = (String) data.nextToken();
+      String[] tt = tagStr.split(" ");
+      String[] words = new String[tt.length];
+      for (int wi=0;wi<words.length;wi++) {
+        words[wi] =
+            tt[wi].substring(0,tt[wi].lastIndexOf('_'));
+      }
+
+      ngramModel.add(new StringList(words), 1, 1);
+    }
+
+    System.out.println("Saving the dictionary");
+
+    ngramModel.cutoff(cutoff, Integer.MAX_VALUE);
+    Dictionary dictionary = ngramModel.toDictionary(true);
+
+    dictionary.serialize(new FileOutputStream(dict));
   }
 
 }
