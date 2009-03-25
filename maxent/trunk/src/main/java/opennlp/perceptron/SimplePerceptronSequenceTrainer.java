79f843b350923832e3b14c56e097347996bb0315
Tom Morton <tsmorton@apache.org>
2009-03-25 23:15:34 +0000

    Fixed to properly do sequence updating.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048109 13f79535-47bb-0310-9956-ffa450edef68

--- maxent/trunk/src/main/java/opennlp/perceptron/SimplePerceptronSequenceTrainer.java
+++ maxent/trunk/src/main/java/opennlp/perceptron/SimplePerceptronSequenceTrainer.java
@@ -48,7 +48,6 @@ public class SimplePerceptronSequenceTrainer {
 
   /** Number of predicates. */
   private int numPreds; 
-  /** Number of outcomes. */
   private int numOutcomes;
 
   /** List of outcomes for each event i, in context[i]. */
@@ -76,11 +75,16 @@ public class SimplePerceptronSequenceTrainer {
   
   private int[] allOutcomesPattern;
   private String[] predLabels;
+  int numSequences;
 
   public AbstractModel trainModel(int iterations, SequenceStream sequenceStream, int cutoff, boolean useAverage) {
     this.iterations = iterations;
     this.sequenceStream = sequenceStream;
     DataIndexer di = new OnePassDataIndexer(new SequenceStreamEventStream(sequenceStream),cutoff,false);
+    numSequences = 0;
+    for (Sequence s : sequenceStream) {
+      numSequences++;
+    }
     outcomeList  = di.getOutcomeList();
     predLabels = di.getPredLabels();
     pmap = new HashMap<String,Integer>();
@@ -179,81 +183,106 @@ public class SimplePerceptronSequenceTrainer {
     int numCorrect = 0;
     int oei=0;
     int si=0;
+    Map<String,Float>[] featureCounts = new Map[numOutcomes];
+    for (int oi=0;oi<numOutcomes;oi++) {
+      featureCounts[oi] = new HashMap<String,Float>();
+    }
+    PerceptronModel model = new PerceptronModel(params,predLabels,pmap,outcomeLabels);
     for (Sequence sequence : sequenceStream) {
-      Event[] taggerEvents = sequenceStream.updateContext(sequence, new PerceptronModel(params,predLabels,pmap,outcomeLabels));
+      Event[] taggerEvents = sequenceStream.updateContext(sequence, model);
       Event[] events = sequence.getEvents();
+      boolean update = false;
       for (int ei=0;ei<events.length;ei++,oei++) {
-        String[] contextStrings = events[ei].getContext();
-        int[] contexts = new int[contextStrings.length];
-        float values[] = events[ei].getValues();
-        for (int ci=0;ci<contexts.length;ci++) {
-          Integer cmi = pmap.get(contextStrings[ci]);
-          contexts[ci] = cmi;
+        if (!taggerEvents[ei].getOutcome().equals(events[ei].getOutcome())) {
+          update = true;
+          //break;
         }
-        int max = omap.get(taggerEvents[ei].getOutcome());
-        boolean correct = max == outcomeList[oei]; 
-        if (correct) {
-          numCorrect ++;
+        else {
+          numCorrect++;
         }
-        for (int oi = 0;oi<numOutcomes;oi++) {
-          if (oi == outcomeList[oei]) {
-            if (!correct) {
-              for (int ci = 0; ci < contexts.length; ci++) {
-                int pi = contexts[ci];
-                if (values == null) {
-                  params[pi].updateParameter(oi, 1);
+      }
+      if (update) {
+        for (int oi=0;oi<numOutcomes;oi++) {
+          featureCounts[oi].clear();
+        }
+        //System.err.print("train:");for (int ei=0;ei<events.length;ei++) {System.err.print(" "+events[ei].getOutcome());} System.err.println();
+        //training feature count computation
+        for (int ei=0;ei<events.length;ei++,oei++) {
+          String[] contextStrings = events[ei].getContext();
+          float values[] = events[ei].getValues();
+          int oi = omap.get(events[ei].getOutcome());
+          for (int ci=0;ci<contextStrings.length;ci++) {
+            float value = 1;
+            if (values != null) {
+              value = values[ci];
+            }
+            Float c = featureCounts[oi].get(contextStrings[ci]);
+            if (c == null) {
+              c = value;
+            }
+            else {
+              c+=value;
+            }
+            featureCounts[oi].put(contextStrings[ci], c);
+          }
+        }
+        //evaluation feature count computation
+        //System.err.print("test: ");for (int ei=0;ei<taggerEvents.length;ei++) {System.err.print(" "+taggerEvents[ei].getOutcome());} System.err.println();
+        for (int ei=0;ei<taggerEvents.length;ei++) {
+          String[] contextStrings = taggerEvents[ei].getContext();
+          float values[] =taggerEvents[ei].getValues();
+          int oi = omap.get(taggerEvents[ei].getOutcome());
+          for (int ci=0;ci<contextStrings.length;ci++) {
+            float value = 1;
+            if (values != null) {
+              value = values[ci];
+            }
+            Float c = featureCounts[oi].get(contextStrings[ci]);
+            if (c == null) {
+              c = -1*value;
+            }
+            else {
+              c-=value;
+            }
+            if (c == 0f) {
+              featureCounts[oi].remove(contextStrings[ci]);
+            }
+            else {
+              featureCounts[oi].put(contextStrings[ci], c);
+            }
+          }
+        }
+        for (int oi=0;oi<numOutcomes;oi++) {
+          for (String feature : featureCounts[oi].keySet()) {
+            Integer pi = pmap.get(feature);
+            if (pi != null) {
+              //System.err.println(si+" "+outcomeLabels[oi]+" "+feature+" "+featureCounts[oi].get(feature));
+              params[pi].updateParameter(oi, featureCounts[oi].get(feature));
+              if (useAverage) {
+                if (updates[pi][oi][VALUE] != 0) {
+                  averageParams[pi].updateParameter(oi,updates[pi][oi][VALUE]*(numSequences*(iteration-updates[pi][oi][ITER])+(si-updates[pi][oi][EVENT])));
+                  //System.err.println("p avp["+pi+"]."+oi+"="+averageParams[pi].getParameters()[oi]);
                 }
-                else {
-                  params[pi].updateParameter(oi, values[ci]);
-                }
-                if (useAverage) {
-                  if (updates[pi][oi][VALUE] != 0) {
-                    averageParams[pi].updateParameter(oi,updates[pi][oi][VALUE]*(numEvents*(iteration-updates[pi][oi][ITER])+(oei-updates[pi][oi][EVENT])));
-                    //System.err.println("p avp["+pi+"]."+oi+"="+averageParams[pi].getParameters()[oi]);
-                  }
-                  //System.err.println("p updates["+pi+"]["+oi+"]=("+updates[pi][oi][ITER]+","+updates[pi][oi][EVENT]+","+updates[pi][oi][VALUE]+") + ("+iteration+","+oei+","+params[pi].getParameters()[oi]+") -> "+averageParams[pi].getParameters()[oi]);
-                  updates[pi][oi][VALUE] = (int) params[pi].getParameters()[oi];
-                  updates[pi][oi][ITER] = iteration;
-                  updates[pi][oi][EVENT] = oei;
-                }
+                //System.err.println("p updates["+pi+"]["+oi+"]=("+updates[pi][oi][ITER]+","+updates[pi][oi][EVENT]+","+updates[pi][oi][VALUE]+") + ("+iteration+","+oei+","+params[pi].getParameters()[oi]+") -> "+averageParams[pi].getParameters()[oi]);
+                updates[pi][oi][VALUE] = (int) params[pi].getParameters()[oi];
+                updates[pi][oi][ITER] = iteration;
+                updates[pi][oi][EVENT] = si;
               }
             }
           }
-          else {
-            if (oi == max) { //case where it correct is taken by above if.
-              for (int ci = 0; ci < contexts.length; ci++) {
-                int pi = contexts[ci];
-                if (values == null) {
-                  params[pi].updateParameter(oi,-1);
-                }
-                else {
-                  params[pi].updateParameter(oi, values[ci]*-1);
-                }
-                if (useAverage) {
-                  if (updates[pi][oi][VALUE] != 0) {
-                    averageParams[pi].updateParameter(oi,updates[pi][oi][VALUE]*(numEvents*(iteration-updates[pi][oi][ITER])+(oei-updates[pi][oi][EVENT])));
-                    //System.err.println(oei+" d avp["+pi+"]."+oi+"="+averageParams[pi].getParameters()[oi]);
-                  }
-                  //System.err.println(oei+" d updates["+pi+"]["+oi+"]=("+updates[pi][oi][ITER]+","+updates[pi][oi][EVENT]+","+updates[pi][oi][VALUE]+") + ("+iteration+","+oei+","+params[pi].getParameters()[oi]+") -> "+averageParams[pi].getParameters()[oi]);
-                  updates[pi][oi][VALUE] = (int) params[pi].getParameters()[oi];
-                  updates[pi][oi][ITER] = iteration;
-                  updates[pi][oi][EVENT] = oei;
-                }
-              }
-            }
-          }
         }
+        model = new PerceptronModel(params,predLabels,pmap,outcomeLabels);
       }
       si++;
     }
     //finish average computation
-    double totIterations = (double) iterations*numEvents;
+    double totIterations = (double) iterations*si;
     if (useAverage && iteration == iterations-1) {
       for (int pi = 0; pi < numPreds; pi++) {
         double[] predParams = averageParams[pi].getParameters();
         for (int oi = 0;oi<numOutcomes;oi++) {
           if (updates[pi][oi][VALUE] != 0) {
-            predParams[oi] +=  updates[pi][oi][VALUE]*(numEvents*(iterations-updates[pi][oi][ITER])-updates[pi][oi][EVENT]);
+            predParams[oi] +=  updates[pi][oi][VALUE]*(numSequences*(iterations-updates[pi][oi][ITER])-updates[pi][oi][EVENT]);
           }
           if (predParams[oi] != 0) {
             predParams[oi] /=totIterations;  
