0f02df51281f601950cbcd32d4b72324ad986264
joernkottmann <joernkottmann@unknown>
2009-05-29 07:47:56 +0000

    Fixed import warning.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048302 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/trunk/src/main/java/opennlp/tools/parser/Parse.java
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/parser/Parse.java
@@ -20,7 +20,6 @@ package opennlp.tools.parser;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
