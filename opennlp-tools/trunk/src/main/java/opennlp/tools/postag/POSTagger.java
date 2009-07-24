ae763ed8157e0b2f9cce1c5bafdac1fea6382da0
Tom Morton <tsmorton@apache.org>
2009-07-24 17:30:26 +0000

    minor updates in licience spelling etc.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048323 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/trunk/src/main/java/opennlp/tools/postag/POSTagger.java
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/postag/POSTagger.java
@@ -1,6 +1,6 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreemnets.  See the NOTICE file distributed with
+ * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
@@ -23,8 +23,6 @@ import opennlp.tools.util.Sequence;
 
 /**
  * The interface for part of speech taggers.
- *
- * @author      Jason Baldridge
  */
 public interface POSTagger {
 
--- opennlp-tools/trunk/src/main/java/opennlp/tools/postag/POSTaggerME.java
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/postag/POSTaggerME.java
@@ -1,6 +1,6 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreemnets.  See the NOTICE file distributed with
+ * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
@@ -18,8 +18,6 @@
 
 package opennlp.tools.postag;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
