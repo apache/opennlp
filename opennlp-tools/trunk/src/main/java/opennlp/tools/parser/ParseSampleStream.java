f38df19540c3b2395450b7b4ebc1a59535280c48
joernkottmann <joernkottmann@unknown>
2009-08-30 21:28:38 +0000

    Initial check-in.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048335 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/parser/ParseSampleStream.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.parser;
+
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.ObjectStreamException;
+
+public class ParseSampleStream implements ObjectStream<Parse> {
+
+  private final ObjectStream<String> in;
+  
+  public ParseSampleStream(ObjectStream<String> in) {
+    this.in = in;
+  }
+
+  public Parse read() throws ObjectStreamException {
+    
+    String parse = in.read();
+    
+    if (parse != null) {
+      return Parse.parseParse(parse);
+    } 
+    else {
+      return null;
+    }
+  }
+  
+  public void reset() throws ObjectStreamException,
+      UnsupportedOperationException {
+    in.reset();
+  }
+}
