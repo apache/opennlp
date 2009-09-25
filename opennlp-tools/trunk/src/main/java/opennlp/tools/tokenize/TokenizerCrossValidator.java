6f9ec5cbef1cd724f52f3088e56523e4b021dd70
joernkottmann <joernkottmann@unknown>
2009-09-25 10:45:07 +0000

    CV support for tokenizer
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048360 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/tokenize/TokenizerCrossValidator.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.tokenize;
+
+import java.io.IOException;
+
+import opennlp.tools.util.CrossValidationPartitioner;
+import opennlp.tools.util.FMeasure;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.ObjectStreamException;
+
+public class TokenizerCrossValidator {
+  
+  private final String language;
+  private final boolean alphaNumericOptimization;
+  
+  private FMeasure fmeasure = new FMeasure();
+  
+  public TokenizerCrossValidator(String language, boolean alphaNumericOptimization) {
+    this.language = language;
+    this.alphaNumericOptimization = alphaNumericOptimization;
+  }
+  
+  public void evaluate(ObjectStream<TokenSample> samples, int nFolds) 
+      throws ObjectStreamException, IOException {
+    
+    CrossValidationPartitioner<TokenSample> partitioner = 
+      new CrossValidationPartitioner<TokenSample>(samples, nFolds);
+  
+     while (partitioner.hasNext()) {
+       
+       CrossValidationPartitioner.TrainingSampleStream<TokenSample> trainingSampleStream =
+         partitioner.next();
+       
+       // Maybe throws IOException if temp file handling fails ...
+       TokenizerModel model = TokenizerME.train(language, trainingSampleStream, 
+           alphaNumericOptimization);
+       
+       TokenizerEvaluator evaluator = new TokenizerEvaluator(new TokenizerME(model));
+       evaluator.evaluate(trainingSampleStream.getTestSampleStream());
+       fmeasure.mergeInto(evaluator.getFMeasure());
+     }
+  }
+  
+  public FMeasure getFMeasure() {
+    return fmeasure;
+  }
+}
