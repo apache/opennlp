ee16bfa1f747b05ffff658d11ff93506b45f6a98
joernkottmann <joernkottmann@unknown>
2009-09-30 11:30:47 +0000

    Added command line util
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048362 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/trunk/src/main/java/opennlp/tools/tokenize/TokenizerCrossValidator.java
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/tokenize/TokenizerCrossValidator.java
@@ -17,12 +17,17 @@
 
 package opennlp.tools.tokenize;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import opennlp.tools.util.CrossValidationPartitioner;
 import opennlp.tools.util.FMeasure;
 import opennlp.tools.util.ObjectStream;
 import opennlp.tools.util.ObjectStreamException;
+import opennlp.tools.util.PlainTextByLineStream;
 
 public class TokenizerCrossValidator {
   
@@ -47,7 +52,7 @@ public class TokenizerCrossValidator {
        CrossValidationPartitioner.TrainingSampleStream<TokenSample> trainingSampleStream =
          partitioner.next();
        
-       // Maybe throws IOException if temp file handling fails ...
+       // Maybe throws IOException if temporary file handling fails ...
        TokenizerModel model = TokenizerME.train(language, trainingSampleStream, 
            alphaNumericOptimization);
        
@@ -59,5 +64,63 @@ public class TokenizerCrossValidator {
   
   public FMeasure getFMeasure() {
     return fmeasure;
+  }
+  
+  private static void usage() {
+    System.err.println("Usage: TokenizerCrossValidator -encoding charset -lang language trainData");
+    System.err.println("-encoding charset specifies the encoding which should be used ");
+    System.err.println("                  for reading and writing text.");
+    System.err.println("-lang language    specifies the language which ");
+    System.err.println("                  is being processed.");
+    System.exit(1);
+  }
+  
+  public static void main(String[] args) throws IOException, ObjectStreamException {
+    int ai=0;
+    String encoding = null;
+    String lang = null;
+    if (args.length != 5) {
+      usage();
+    }
+    
+    while (args[ai].startsWith("-")) {
+      if (args[ai].equals("-encoding")) {
+        ai++;
+        if (ai < args.length) {
+          encoding = args[ai];
+          ai++;
+        }
+        else {
+          usage();
+        }
+      }
+      else if (args[ai].equals("-lang")) {
+        ai++;
+        if (ai < args.length) {
+          lang = args[ai];
+          ai++;
+        }
+        else {
+          usage();
+        }
+      }
+      else {
+        usage();
+      }
+    }
+    
+    File trainingDataFile = new File(args[ai++]);
+    
+    FileInputStream trainingDataIn = new FileInputStream(trainingDataFile);
+    ObjectStream<String> lineStream = new PlainTextByLineStream(trainingDataIn.getChannel(), encoding);
+    ObjectStream<TokenSample> sampleStream = new TokenSampleStream(lineStream);
+    
+    TokenizerCrossValidator validator = new TokenizerCrossValidator(lang, false);
+    
+    validator.evaluate(sampleStream, 10);
+    
+    FMeasure result = validator.getFMeasure();
+    
+    System.out.println(result.toString());
   }
 }
