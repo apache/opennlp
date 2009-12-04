9ecf8e5b366e0e20308fc4847856c47be4a23b9e
joernkottmann <joernkottmann@unknown>
2009-12-04 12:12:04 +0000

    Initial check in
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048391 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/cmdline/namefind/TokenNameFinder.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.cmdline.namefind;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import opennlp.tools.cmdline.CLI;
+import opennlp.tools.cmdline.CmdLineTool;
+import opennlp.tools.cmdline.CmdLineUtil;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.NameSample;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.Span;
+
+public class TokenNameFinder implements CmdLineTool {
+
+  public String getName() {
+    return "TokenNameFinder";
+  }
+  
+  public String getShortDescription() {
+    return "";
+  }
+  
+  public String getHelp() {
+    return "Usage: " + CLI.CMD + " " + getName() + " model < sentences";
+  }
+  
+  static TokenNameFinderModel loadModel(File modelFile) {
+    
+    CmdLineUtil.checkInputFile("Token Name Finder model", modelFile);
+
+    System.err.print("Loading model ... ");
+    
+    TokenNameFinderModel model;
+    try {
+      InputStream modelIn = new FileInputStream(modelFile);
+      model = new TokenNameFinderModel(modelIn);
+      modelIn.close();
+    }
+    catch (IOException e) {
+      System.err.println("failed");
+      System.err.println("IO error while loading model: " + e.getMessage());
+      System.exit(-1);
+      return null;
+    }
+    catch (InvalidFormatException e) {
+      System.err.println("failed");
+      System.err.println("Model has invalid format: " + e.getMessage());
+      System.exit(-1);
+      return null;
+    }
+    
+    System.err.println("done");
+    
+    return model;
+  }
+  
+  public void run(String[] args) {
+    
+    if (args.length != 1) {
+      System.out.println(getHelp());
+      System.exit(1);
+    }
+    
+    TokenNameFinderModel model = loadModel(new File(args[0]));
+
+    NameFinderME nameFinder = new NameFinderME(model);
+    
+    ObjectStream<String> untokenizedLineStream =
+        new PlainTextByLineStream(new InputStreamReader(System.in));
+    
+    try {
+      String line;
+      while((line = untokenizedLineStream.read()) != null) {
+        String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE.tokenize(line);
+        
+        Span names[] = nameFinder.find(whitespaceTokenizerLine);
+        
+        NameSample nameSample = new NameSample(whitespaceTokenizerLine, names, false);
+        
+        System.out.println(nameSample.toString());
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/cmdline/namefind/TokenNameFinderEvaluator.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.cmdline.namefind;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import opennlp.tools.cmdline.CLI;
+import opennlp.tools.cmdline.CmdLineTool;
+import opennlp.tools.cmdline.CmdLineUtil;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.NameSampleDataStream;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+
+public class TokenNameFinderEvaluator implements CmdLineTool {
+
+  public String getName() {
+    return "TokenNameFinderEvaluator";
+  }
+  
+  public String getShortDescription() {
+    return "";
+  }
+  
+  public String getHelp() {
+    return "Usage: " + CLI.CMD + " " + getName() + " -encoding charset model testData";
+  }
+
+  public void run(String[] args) {
+    try {
+      if (args.length != 4) {
+        System.out.println(getHelp());
+        System.exit(1);
+      }
+      
+      File testData = new File(args[3]);
+      CmdLineUtil.checkInputFile("Test data", testData);
+      
+      String encoding = CmdLineUtil.getEncodingParameter(args);
+      
+      if (encoding == null) {
+        System.out.println(getHelp());
+        System.exit(1);
+      }
+      
+      TokenNameFinderModel model = TokenNameFinder.loadModel(new File(args[2]));
+      
+      opennlp.tools.namefind.TokenNameFinderEvaluator evaluator = 
+          new opennlp.tools.namefind.TokenNameFinderEvaluator(new NameFinderME(model));
+      
+      System.out.print("Evaluating ... ");
+      ObjectStream<String> sampleStringStream = new PlainTextByLineStream(
+          new InputStreamReader(new FileInputStream(testData), encoding));
+      
+      evaluator.evaluate(new NameSampleDataStream(sampleStringStream));
+      sampleStringStream.close();
+      System.out.println("done");
+      
+      System.out.println();
+      
+      System.out.println(evaluator.getFMeasure());
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
