2c5bb493c3ed9205de3a0e2c0d5efa565ac8e404
joernkottmann <joernkottmann@unknown>
2009-12-10 10:48:15 +0000

    Initial check in
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048415 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/namefind/TokenNameFinderCrossValidator.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package opennlp.tools.namefind;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import opennlp.tools.util.CrossValidationPartitioner;
+import opennlp.tools.util.FMeasure;
+import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.ObjectStreamException;
+
+public class TokenNameFinderCrossValidator {
+
+  private final String language;
+  private FMeasure fmeasure = new FMeasure();
+  
+  public TokenNameFinderCrossValidator(String language) {
+    this.language = language;
+  }
+  
+  public void evaluate(ObjectStream<NameSample> samples, int nFolds) throws ObjectStreamException,
+      InvalidFormatException, IOException {
+    CrossValidationPartitioner<NameSample> partitioner = 
+        new CrossValidationPartitioner<NameSample>(samples, nFolds);
+    
+    while (partitioner.hasNext()) {
+      
+      CrossValidationPartitioner.TrainingSampleStream<NameSample> trainingSampleStream =
+          partitioner.next();
+      
+      TokenNameFinderModel model = NameFinderME.train(language, trainingSampleStream,
+          Collections.<String, Object>emptyMap());
+       
+       // do testing
+       TokenNameFinderEvaluator evaluator = new TokenNameFinderEvaluator(
+           new NameFinderME(model));
+
+       evaluator.evaluate(trainingSampleStream.getTestSampleStream());
+       
+       fmeasure.mergeInto(evaluator.getFMeasure());
+     }
+  }
+  
+  public FMeasure getFMeasure() {
+    return fmeasure;
+  }
+}
