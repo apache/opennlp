f99267797741440e10048e96469a42b58f60dca3
joernkottmann <joernkottmann@unknown>
2010-08-12 08:50:11 +0000

    Corrected typo.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048720 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/trunk/src/main/java/opennlp/tools/util/SequenceValidator.java
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/util/SequenceValidator.java
@@ -21,7 +21,7 @@ public interface SequenceValidator<T> {
 
   /**
    * Determines whether a particular continuation of a sequence is valid.
-   * This is used to restrict invalid sequences such as thoses used in start/continue tag-based chunking
+   * This is used to restrict invalid sequences such as those used in start/continue tag-based chunking
    * or could be used to implement tag dictionary restrictions.
    *
    * @param i The index in the input sequence for which the new outcome is being proposed.
