e2efc14455783660cce2889027a635acf43395f6
joernkottmann <joernkottmann@unknown>
2010-09-01 08:00:53 +0000

    Initial check in
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048745 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/formats/POSToTokenSampleStream.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.formats;
+
+import java.io.IOException;
+
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.tokenize.Detokenizer;
+import opennlp.tools.tokenize.TokenSample;
+import opennlp.tools.util.ObjectStream;
+
+public class POSToTokenSampleStream implements ObjectStream<TokenSample> {
+
+  private final Detokenizer detokenizer;
+  
+  private final ObjectStream<POSSample> samples;
+  
+  public POSToTokenSampleStream(Detokenizer detokenizer, ObjectStream<POSSample> samples) {
+    this.detokenizer = detokenizer;
+    this.samples = samples;
+  }
+  
+  public TokenSample read() throws IOException {
+    
+    POSSample posSample = samples.read();
+    
+    TokenSample tokenSample = null;
+    
+    if (posSample != null ) {
+      tokenSample = new TokenSample(detokenizer, posSample.getSentence());
+    }
+    
+    return tokenSample;
+  }
+
+  public void reset() throws IOException, UnsupportedOperationException {
+    samples.reset();
+  }
+
+  public void close() throws IOException {
+    samples.close();
+  }
+}
