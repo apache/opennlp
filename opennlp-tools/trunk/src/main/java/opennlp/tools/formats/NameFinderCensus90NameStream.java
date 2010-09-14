e2a9038c1e415fc36dc74199756fd8d29545afc7
jameskosin <jameskosin@unknown>
2010-09-14 02:53:06 +0000

    Stream sampler for the Census data 1990 format.  This allows us to add names to the dictionary from collected names.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/tags/asf_migration@1048793 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/trunk/src/main/java/opennlp/tools/formats/NameFinderCensus90NameStream.java
@@ -0,0 +1,92 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package opennlp.tools.formats;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.StringList;
+
+/**
+ *
+ * @author James Kosin
+ */
+public class NameFinderCensus90NameStream implements ObjectStream<StringList> {
+
+  private final Locale locale;
+  private final Charset encoding;
+  private final ObjectStream<String> lineStream;
+
+  public NameFinderCensus90NameStream(String lang, ObjectStream<String> lineStream) {
+      this.locale = new Locale(lang);
+      this.encoding = Charset.defaultCharset();
+      // todo how do we find the encoding for an already open ObjectStream() ?
+      this.lineStream = null;
+  }
+
+  public NameFinderCensus90NameStream(String lang, InputStream in, String encoding) {
+      this.locale = new Locale(lang);
+      this.encoding = Charset.forName(encoding);
+      try {
+          this.lineStream = new PlainTextByLineStream(in, this.encoding.name());
+      } catch (UnsupportedEncodingException e) {
+          throw new IllegalStateException(e);
+      }
+  }
+
+  public StringList read() throws IOException {
+      String line = lineStream.read();
+      StringList name = null;
+
+      if ((line != null) &&
+          (!line.isEmpty())) {
+          String name2;
+          // find the location of the name separator in the line of data.
+          int pos = line.indexOf(' ');
+          if ((pos != -1)) {
+              String parsed = line.substring(0, pos);
+              // the data is in ALL CAPS ... so the easiest way is to convert
+              // back to standard mixed case.
+              if ((parsed.length() > 2) &&
+                  (parsed.startsWith("MC"))) {
+                  name2 = parsed.substring(0,1).toUpperCase(locale) +
+                          parsed.substring(1,2).toLowerCase(locale) +
+                          parsed.substring(2,3).toUpperCase(locale) +
+                          parsed.substring(3).toLowerCase(locale);
+              } else {
+                  name2 = parsed.substring(0,1).toUpperCase(locale) +
+                          parsed.substring(1).toLowerCase(locale);
+              }
+              name = new StringList(new String[]{name2});
+          }
+      }
+
+      return name;
+  }
+
+  public void reset() throws IOException, UnsupportedOperationException {
+      lineStream.reset();
+  }
+
+  public void close() throws IOException {
+      lineStream.close();
+  }
+
+}
