304362b18875b0c7071fcc1baa061945ccc25c52
Jörn Kottmann <joern@apache.org>
2010-12-16 16:37:48 +0000

    OPENNLP-22: Removed author tags and cvs tags
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1050048 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/formats/Conll03NameSampleStream.java
+++ opennlp-tools/src/main/java/opennlp/tools/formats/Conll03NameSampleStream.java
@@ -1,6 +1,4 @@
 /*
- *  Copyright 2010 James Kosin.
- * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -30,8 +28,6 @@ import opennlp.tools.util.Span;
 import opennlp.tools.util.StringUtil;
 
 /**
- *
- * @author James Kosin
  */
 public class Conll03NameSampleStream implements ObjectStream<NameSample>{
 
