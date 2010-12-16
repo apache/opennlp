d7cdea8dcd8a0f4822fee8559db7bd9eeace6cb8
Jorn Kottmann <joern@apache.org>
2010-12-16 16:45:10 +0000

    OPENNLP-22: Removed author tags and cvs tags
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1050052 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/test/java/opennlp/tools/formats/NameFinderCensus90NameStreamTest.java
+++ opennlp-tools/src/test/java/opennlp/tools/formats/NameFinderCensus90NameStreamTest.java
@@ -1,6 +1,4 @@
 /*
- *  Copyright 2010 James Kosin.
- * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -25,10 +23,6 @@ import opennlp.tools.util.StringList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author James Kosin
- */
 public class NameFinderCensus90NameStreamTest {
 
   private static ObjectStream<StringList> openData(String name) throws IOException {
