66478bf58748877fb70ace80fe4cd60fd114add7
William Daniel Colen de Moura Silva <colen@apache.org>
2011-01-05 17:32:11 +0000

    OPENNLP-30: Added code and test for Chunker Evaluator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1055544 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/test/java/opennlp/tools/chunker/ChunkerEvaluatorTest.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.chunker;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.eval.FMeasure;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link ChunkerEvaluator}.
+ * 
+ * @see ChunkerEvaluator
+ */
+public class ChunkerEvaluatorTest {
+	
+	private static final double DELTA = 1.0E-9d;
+
+	/**
+	 * Checks the evaluator results against the results got using the conlleval,
+	 * available at http://www.cnts.ua.ac.be/conll2000/chunking/output.html
+	 * The output.txt file has only 3 sentences, but can be replaced by the one
+	 * available at the conll2000 site to validate using a bigger sample.
+	 * @throws IOException
+	 */
+	@Test
+	public void testEvaluator() throws IOException {
+		InputStream inPredicted = getClass().getClassLoader().getResourceAsStream(
+				"opennlp/tools/chunker/output.txt");
+		InputStream inExpected = getClass().getClassLoader().getResourceAsStream(
+		"opennlp/tools/chunker/output.txt");
+
+		String encoding = "UTF-8";
+
+		DummyChunkSampleStream predictedSample = new DummyChunkSampleStream(
+				new PlainTextByLineStream(new InputStreamReader(inPredicted, encoding)), true);
+		
+		DummyChunkSampleStream expectedSample = new DummyChunkSampleStream(
+				new PlainTextByLineStream(new InputStreamReader(inExpected)), false);
+		
+		Chunker dummyChunker = new DummyChunker(predictedSample);
+		ChunkerEvaluator evaluator = new ChunkerEvaluator(dummyChunker);
+		
+		evaluator.evaluate(expectedSample);
+		
+		FMeasure fm = evaluator.getFMeasure();
+		
+		assertEquals(0.8d, fm.getPrecisionScore(), DELTA);
+		assertEquals(0.875d, fm.getRecallScore(), DELTA);
+	}
+
+}
---
+++ opennlp-tools/src/test/java/opennlp/tools/chunker/DummyChunker.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.chunker;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import opennlp.tools.util.Sequence;
+
+/**
+ * This dummy chunker implementation reads a file formatted as described at
+ * <a hraf="http://www.cnts.ua.ac.be/conll2000/chunking/output.html/">] to
+ * simulate a Chunker. The file has samples of sentences, with target and
+ * predicted values.
+ */
+public class DummyChunker implements Chunker {
+
+	private DummyChunkSampleStream mSampleStream;
+
+	public DummyChunker(DummyChunkSampleStream aSampleStream) {
+		mSampleStream = aSampleStream;
+	}
+
+	public List<String> chunk(List<String> toks, List<String> tags) {
+		return Arrays.asList(chunk(toks.toArray(new String[toks.size()]),
+				tags.toArray(new String[tags.size()])));
+	}
+
+	public String[] chunk(String[] toks, String[] tags) {
+		try {
+			ChunkSample predsSample = mSampleStream.read();
+
+			// checks if the streams are sync
+			for (int i = 0; i < toks.length; i++) {
+				if (!toks[i].equals(predsSample.getSentence()[i])
+						|| !tags[i].equals(predsSample.getTags()[i])) {
+					throw new RuntimeException("The streams are not sync!"
+							+ "\n expected sentence: " + Arrays.toString(toks)
+							+ "\n expected tags: " + Arrays.toString(tags)
+							+ "\n predicted sentence: "
+							+ Arrays.toString(predsSample.getSentence())
+							+ "\n predicted tags: "
+							+ Arrays.toString(predsSample.getTags()));
+				}
+			}
+
+			return predsSample.getPreds();
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public Sequence[] topKSequences(List<String> sentence, List<String> tags) {
+		return null;
+	}
+
+	public Sequence[] topKSequences(String[] sentence, String[] tags,
+			double minSequenceScore) {
+		return null;
+	}
+
+}
