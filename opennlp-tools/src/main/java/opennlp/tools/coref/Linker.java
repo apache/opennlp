7f34458af53d47e75f1ebb1395cdbb5df10a00f9
Jörn Kottmann <joern@apache.org>
2011-01-18 23:13:04 +0000

    OPENNLP-24: Formated comments
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1060617 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/coref/Linker.java
+++ opennlp-tools/src/main/java/opennlp/tools/coref/Linker.java
@@ -24,28 +24,45 @@ import opennlp.tools.coref.mention.Mention;
 import opennlp.tools.coref.mention.MentionContext;
 import opennlp.tools.coref.mention.MentionFinder;
 
-/** A linker provides an interface for finding mentions, {@link #getMentionFinder getMentionFinder},
+/** 
+ * A linker provides an interface for finding mentions, {@link #getMentionFinder getMentionFinder},
  * and creating entities out of those mentions, {@link #getEntities getEntities}.  This interface also allows
  * for the training of a resolver with the method {@link #setEntities setEntitites} which is used to give the
  * resolver mentions whose entityId fields indicate which mentions refer to the same entity and the
  * {@link #train train} method which compiles all the information provided via calls to
  * {@link #setEntities setEntities} into a model.
- *
  */
 public interface Linker {
 
 
-  /** String constant used to label a mention which is a description. */
+  /** 
+   * String constant used to label a mention which is a description.
+   */
   public static final String DESCRIPTOR = "desc";
-  /** String constant used to label an mention in an appositive relationship. */
+  
+  /** 
+   * String constant used to label an mention in an appositive relationship.
+   */
   public static final String ISA = "isa";
-  /** String constant used to label a mention which consists of two or more noun phrases. */
+  
+  /** 
+   * String constant used to label a mention which consists of two or more noun phrases.
+   */
   public static final String COMBINED_NPS = "cmbnd";
-  /** String constant used to label a mention which consists of a single noun phrase. */
+  
+  /** 
+   * String constant used to label a mention which consists of a single noun phrase.
+   */
   public static final String NP = "np";
-  /** String constant used to label a mention which is a proper noun modifying another noun. */
+  
+  /** 
+   * String constant used to label a mention which is a proper noun modifying another noun.
+   */
   public static final String PROPER_NOUN_MODIFIER = "pnmod";
-  /** String constant used to label a mention which is a pronoun. */
+  
+  /** 
+   * String constant used to label a mention which is a pronoun.
+   */
   public static final String PRONOUN_MODIFIER = "np";
 
  
@@ -53,12 +70,14 @@ public interface Linker {
    * Indicated that the specified mentions can be used to train this linker.
    * This requires that the coreference relationship between the mentions have been labeled
    * in the mention's id field.
+   * 
    * @param mentions The mentions to be used to train the linker.
    */
   public void setEntities(Mention[] mentions);
 
   /** Returns a list of entities which group the mentions into entity classes.
    * @param mentions A array of mentions.
+   * 
    * @return An array of discourse entities.
    */
   public DiscourseEntity[] getEntities(Mention[] mentions);
@@ -66,23 +85,28 @@ public interface Linker {
   /**
    * Creates mention contexts for the specified mention exents.  These are used to compute coreference features over.
    * @param mentions The mention of a document.
+   * 
    * @return mention contexts for the specified mention exents.
    */
   public MentionContext[] constructMentionContexts(Mention[] mentions);
 
-  /** Trains the linker based on the data specified via calls to {@link #setEntities setEntities}.
+  /** 
+   * Trains the linker based on the data specified via calls to {@link #setEntities setEntities}.
+   *
    * @throws IOException
    */
   public void train() throws IOException;
 
   /**
    * Returns the mention finder for this linker.  This can be used to get the mentions of a Parse.
+   * 
    * @return The object which finds mentions for this linker.
    */
   public MentionFinder getMentionFinder();
 
   /**
    * Returns the head finder associated with this linker.
+   * 
    * @return The head finder associated with this linker.
    */
   public HeadFinder getHeadFinder();
