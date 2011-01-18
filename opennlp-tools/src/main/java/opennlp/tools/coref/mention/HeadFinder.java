1a3cdb0eb756227d664ee20c485a52b8f470e2c1
Jörn Kottmann <joern@apache.org>
2011-01-18 21:57:49 +0000

    OPENNLP-24: Formated comments
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1060592 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/coref/mention/HeadFinder.java
+++ opennlp-tools/src/main/java/opennlp/tools/coref/mention/HeadFinder.java
@@ -22,28 +22,35 @@ package opennlp.tools.coref.mention;
  */
 public interface HeadFinder {
 
-  /** Returns the child parse which contains the lexical head of the specified parse.
+  /** 
+   * Returns the child parse which contains the lexical head of the specified parse.
+   * 
    * @param parse The parse in which to find the head.
    * @return The parse containing the lexical head of the specified parse.  If no head is
    * available or the constituent has no sub-components that are eligible heads then null is returned.
    */
   public Parse getHead(Parse parse);
 
-  /** Returns which index the specified list of token is the head word.
+  /** 
+   * Returns which index the specified list of token is the head word.
+   * 
    * @param parse The parse in which to find the head index.
    * @return The index of the head token.
    */
   public int getHeadIndex(Parse parse);
 
-  /** Returns the parse bottom-most head of a <code>Parse</code>.  If no
-   * head is available which is a child of <code>p</code> then
-   *  <code>p</code> is returned.
-   *  @param p Parse to find the head of.
-   *  @return bottom-most head of p.
+  /** 
+   * Returns the parse bottom-most head of a <code>Parse</code>. If no
+   * head is available which is a child of <code>p</code> then <code>p</code> is returned.
+   * 
+   * @param p Parse to find the head of.
+   * @return bottom-most head of p.
    */
   public Parse getLastHead(Parse p);
 
-  /** Returns head token for the specified np parse.
+  /** 
+   * Returns head token for the specified np parse.
+   * 
    * @param np The noun parse to get head from.
    * @return head token parse.
    */
