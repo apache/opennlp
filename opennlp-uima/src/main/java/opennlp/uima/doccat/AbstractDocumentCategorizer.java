594d019c7af42d5c1f2cd87e284ec7c22f6aafd5
Jorn Kottmann <joern@apache.org>
2011-01-19 15:27:57 +0000

    OpenNLP-51 Extended the integration with an AE which can set the doccat category label as language.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1060835 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-uima/src/main/java/opennlp/uima/doccat/AbstractDocumentCategorizer.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreemnets.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.uima.doccat;
+
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+import opennlp.uima.util.AnnotatorUtil;
+import opennlp.uima.util.UimaUtil;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.CasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.resource.ResourceAccessException;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Level;
+import org.apache.uima.util.Logger;
+
+/**
+ * Abstract document categorizer which can be implemented to define how the
+ * output of the categorizer should be written into the CAS.
+ */
+abstract class AbstractDocumentCategorizer extends CasAnnotator_ImplBase {
+
+  private UimaContext context;
+
+  private Logger mLogger;
+
+  private opennlp.tools.doccat.DocumentCategorizer mCategorizer;
+
+  private Type mTokenType;
+
+  public void initialize(UimaContext context)
+      throws ResourceInitializationException {
+
+    super.initialize(context);
+
+    this.context = context;
+
+    mLogger = context.getLogger();
+
+    if (mLogger.isLoggable(Level.INFO)) {
+      mLogger.log(Level.INFO, "Initializing the OpenNLP Categorizer.");
+    }
+
+    DoccatModel model;
+
+    try {
+      DoccatModelResource modelResource = (DoccatModelResource) context
+          .getResourceObject(UimaUtil.MODEL_PARAMETER);
+
+      model = modelResource.getModel();
+    } catch (ResourceAccessException e) {
+      throw new ResourceInitializationException(e);
+    }
+
+    mCategorizer = new DocumentCategorizerME(model);
+  }
+  
+  public void typeSystemInit(TypeSystem typeSystem) 
+      throws AnalysisEngineProcessException {
+    mTokenType = AnnotatorUtil.getRequiredTypeParameter(context, typeSystem,
+        UimaUtil.SENTENCE_TYPE_PARAMETER);
+  }
+  
+  protected abstract void setBestCategory(CAS cas, String bestCategory);
+  
+  public void process(CAS cas) {
+    
+    double result[];
+    
+    if (mTokenType != null) {
+      // TODO:
+      // count tokens
+      // create token array
+      // pass array to doccat
+      // create result annotation
+      result = mCategorizer.categorize(cas.getDocumentText());
+    }
+    else {
+      result = mCategorizer.categorize(cas.getDocumentText());
+    }
+    
+    String bestCategory = mCategorizer.getBestCategory(result);
+    
+    setBestCategory(cas, bestCategory);
+  }
+}
---
+++ opennlp-uima/src/main/java/opennlp/uima/doccat/LanguageDetector.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreemnets.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.uima.doccat;
+
+import org.apache.uima.cas.CAS;
+
+/**
+ * Analysis Engine which can detected the language of a text. The AE uses the OpenNLP document
+ * categorizer and a special language detection model. The outcome of the document categorizer
+ * model is written into the language field of the CAS view.
+ */
+public class LanguageDetector extends AbstractDocumentCategorizer {
+
+  @Override
+  protected void setBestCategory(CAS cas, String bestCategory) {
+    cas.setDocumentLanguage(bestCategory);
+  }
+}
