bf419bc1fe677a7edb362653c9ade390ba1ae04d
Jörn Kottmann <joern@apache.org>
2011-01-19 15:27:57 +0000

    OpenNLP-51 Extended the integration with an AE which can set the doccat category label as language.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1060835 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-uima/src/main/java/opennlp/uima/doccat/DocumentCategorizer.java
+++ opennlp-uima/src/main/java/opennlp/uima/doccat/DocumentCategorizer.java
@@ -17,13 +17,8 @@
 
 package opennlp.uima.doccat;
 
-import opennlp.tools.doccat.DoccatModel;
-import opennlp.tools.doccat.DocumentCategorizerME;
 import opennlp.uima.util.AnnotatorUtil;
-import opennlp.uima.util.UimaUtil;
 
-import org.apache.uima.UimaContext;
-import org.apache.uima.analysis_component.CasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.FSIndex;
@@ -31,94 +26,34 @@ import org.apache.uima.cas.Feature;
 import org.apache.uima.cas.Type;
 import org.apache.uima.cas.TypeSystem;
 import org.apache.uima.cas.text.AnnotationFS;
-import org.apache.uima.resource.ResourceAccessException;
-import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.util.Level;
-import org.apache.uima.util.Logger;
 
 /**
- * OpenNLP NameFinder trainer.
+ * OpenNLP Document Categorizer.
  * 
  * Mandatory parameters:
  */
-public class DocumentCategorizer extends CasAnnotator_ImplBase {
+public class DocumentCategorizer extends AbstractDocumentCategorizer {
   
-  private UimaContext context;
-	
-  private Logger mLogger;
-  
-  private opennlp.tools.doccat.DocumentCategorizer mCategorizer;
-
-  private Type mTokenType;
-
   private Type mCategoryType;
 
   private Feature mCategoryFeature;
   
-  public void initialize(UimaContext context) 
-      throws ResourceInitializationException {
-    
-    super.initialize(context);
-    
-    this.context = context;
-	  
-	mLogger = context.getLogger();
-	  
-    if (mLogger.isLoggable(Level.INFO)) {
-      mLogger.log(Level.INFO, "Initializing the OpenNLP Categorizer.");
-    }  
-    
-    DoccatModel model;
-    
-    try {
-      DoccatModelResource modelResource = 
-            (DoccatModelResource) context.getResourceObject(UimaUtil.MODEL_PARAMETER);
-        
-        model = modelResource.getModel();
-    }
-    catch (ResourceAccessException e) {
-        throw new ResourceInitializationException(e);
-    }
-    
-    mCategorizer = new DocumentCategorizerME(model);
-  }
+
   
   public void typeSystemInit(TypeSystem typeSystem) 
       throws AnalysisEngineProcessException {
     
-    // yes it must, the user later would use a very simple tokenizer and pass it to the
-    // doccat for language detection
-	  mTokenType = AnnotatorUtil.getRequiredTypeParameter(context, typeSystem,
-        UimaUtil.SENTENCE_TYPE_PARAMETER);
-    
     // get category type and feature (it a document propery, one object with a feature)
-    mCategoryType = AnnotatorUtil.getRequiredTypeParameter(context, typeSystem,
+    mCategoryType = AnnotatorUtil.getRequiredTypeParameter(getContext(), typeSystem,
         "opennlp.uima.doccat.CategoryType");
     
     // get feature name
-    mCategoryFeature = AnnotatorUtil.getRequiredFeatureParameter(context, mCategoryType, 
+    mCategoryFeature = AnnotatorUtil.getRequiredFeatureParameter(getContext(), mCategoryType, 
     		"opennlp.uima.doccat.CategoryFeature", CAS.TYPE_NAME_STRING);
   }
   
-  public void process(CAS tcas) {
-    
-    double result[];
-    
-    if (mTokenType != null) {
-      // TODO:
-      // count tokens
-      // create token array
-      // pass array to doccat
-      // create result annotation
-      result = mCategorizer.categorize(tcas.getDocumentText());
-    }
-    else {
-      result = mCategorizer.categorize(tcas.getDocumentText());
-    }
-    
-    String bestCategroy = mCategorizer.getBestCategory(result);
-    
-    // get cat fs 
+  @Override
+  protected void setBestCategory(CAS tcas, String bestCategory) {
     FSIndex<AnnotationFS> categoryIndex = tcas.getAnnotationIndex(mCategoryType);
     
     AnnotationFS categoryAnnotation = (AnnotationFS) (categoryIndex.size() > 0 ? 
@@ -134,6 +69,6 @@ public class DocumentCategorizer extends CasAnnotator_ImplBase {
       tcas.getIndexRepository().addFS(categoryAnnotation);
     }    
     
-    categoryAnnotation.setStringValue(mCategoryFeature, bestCategroy);
+    categoryAnnotation.setStringValue(mCategoryFeature, bestCategory);
   }
 }
