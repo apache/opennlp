8209edc7ddccb801bb6e0a8e3b465babc44fb89d
Jorn Kottmann <joern@apache.org>
2011-01-25 15:12:41 +0000

    OPENLP-24 Formated code to comply with conventions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1063314 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-maxent/src/test/java/opennlp/maxent/io/RealValueFileEventStreamTest.java
+++ opennlp-maxent/src/test/java/opennlp/maxent/io/RealValueFileEventStreamTest.java
@@ -25,13 +25,16 @@ import opennlp.model.RealValueFileEventStream;
 
 public class RealValueFileEventStreamTest extends TestCase {
 
-	public void testLastLineBug() throws IOException {
-		RealValueFileEventStream rvfes = new RealValueFileEventStream("src/test/resources/data/opennlp/maxent/io/rvfes-bug-data-ok.txt");
-		OnePassRealValueDataIndexer indexer = new OnePassRealValueDataIndexer(rvfes, 1);
-		assertEquals(1, indexer.getOutcomeLabels().length);
-		
-		rvfes = new RealValueFileEventStream("src/test/resources/data/opennlp/maxent/io/rvfes-bug-data-broken.txt");
-		indexer = new OnePassRealValueDataIndexer(rvfes, 1);
-		assertEquals(1, indexer.getOutcomeLabels().length);
-	}
+  public void testLastLineBug() throws IOException {
+    RealValueFileEventStream rvfes = new RealValueFileEventStream(
+        "src/test/resources/data/opennlp/maxent/io/rvfes-bug-data-ok.txt");
+    OnePassRealValueDataIndexer indexer = new OnePassRealValueDataIndexer(
+        rvfes, 1);
+    assertEquals(1, indexer.getOutcomeLabels().length);
+
+    rvfes = new RealValueFileEventStream(
+        "src/test/resources/data/opennlp/maxent/io/rvfes-bug-data-broken.txt");
+    indexer = new OnePassRealValueDataIndexer(rvfes, 1);
+    assertEquals(1, indexer.getOutcomeLabels().length);
+  }
 }
