b7183a3d1a5ca1840fb9a6cfc30716077c277c8f
Jörn Kottmann <joern@apache.org>
2011-05-19 12:23:39 +0000

    OPENNLP-181 Updated version to 1.5.2
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1124708 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/util/Version.java
+++ opennlp-tools/src/main/java/opennlp/tools/util/Version.java
@@ -139,6 +139,6 @@ public class Version {
    * @return the current version
    */
   public static Version currentVersion() {
-    return new Version(1, 5, 1);
+    return new Version(1, 5, 2);
   }
 }
