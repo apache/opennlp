6a48914d6a65fce57d6bd9527a7f3096c8ea538b
Jörn Kottmann <joern@apache.org>
2011-05-19 10:11:45 +0000

    OPENNLP-180 Removed old main methods
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1124657 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/sentdetect/SDCrossValidator.java
+++ opennlp-tools/src/main/java/opennlp/tools/sentdetect/SDCrossValidator.java
@@ -92,16 +92,4 @@ public class SDCrossValidator {
   public FMeasure getFMeasure() {
     return fmeasure;
   }
-  
-  @Deprecated
-  public static void main(String[] args) throws Exception {
-    
-    SDCrossValidator cv = new SDCrossValidator("en");
-    
-    cv.evaluate(new SentenceSampleStream(new PlainTextByLineStream(
-       new FileInputStream("/home/joern/Infopaq/opennlp.data/en/eos/eos.all").getChannel(),
-        "ISO-8859-1")), 10);
-    
-    System.out.println(cv.getFMeasure().toString());
-  }
 }
