6929da54d08a0d038531d287232e13b8b760a00a
William Daniel Colen de Moura Silva <colen@apache.org>
2011-07-21 20:36:15 +0000

    OPENNLP-236 Added command line tool to create dictionaries.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1149348 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/cmdline/CLI.java
+++ opennlp-tools/src/main/java/opennlp/tools/cmdline/CLI.java
@@ -30,6 +30,7 @@ import opennlp.tools.cmdline.chunker.ChunkerCrossValidatorTool;
 import opennlp.tools.cmdline.chunker.ChunkerEvaluatorTool;
 import opennlp.tools.cmdline.chunker.ChunkerMETool;
 import opennlp.tools.cmdline.chunker.ChunkerTrainerTool;
+import opennlp.tools.cmdline.dictionary.DictionaryBuilderTool;
 import opennlp.tools.cmdline.doccat.DoccatConverterTool;
 import opennlp.tools.cmdline.doccat.DoccatTool;
 import opennlp.tools.cmdline.doccat.DoccatTrainerTool;
@@ -76,6 +77,9 @@ public final class CLI {
     tools.add(new DoccatTool());
     tools.add(new DoccatTrainerTool());
     tools.add(new DoccatConverterTool());
+    
+    // Dictionary Builder
+    tools.add(new DictionaryBuilderTool());
     
     // Tokenizer
     tools.add(new SimpleTokenizerTool());
