d8af9b10fd68e313e14945adeb1b1c8a9c9f0977
William Colen <colen@apache.org>
2011-08-19 01:11:12 +0000

    OPENNLP-256 Added initial code for Detailed FMeasure evaluator listener.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1159467 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/test/java/opennlp/tools/chunker/ChunkerDetailedFMeasureListenerTest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.chunker;
+
+import static junit.framework.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+
+import opennlp.tools.cmdline.chunker.ChunkerDetailedFMeasureListener;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.eval.EvaluationSampleListener;
+
+import org.junit.Test;
+
+public class ChunkerDetailedFMeasureListenerTest {
+
+  @Test
+  public void testEvaluator() throws IOException {
+    InputStream inPredicted = getClass().getClassLoader().getResourceAsStream(
+        "opennlp/tools/chunker/output.txt");
+    InputStream inExpected = getClass().getClassLoader().getResourceAsStream(
+        "opennlp/tools/chunker/output.txt");
+    
+    InputStream detailedOutputStream = getClass().getClassLoader().getResourceAsStream(
+    "opennlp/tools/chunker/detailedOutput.txt");
+
+    String encoding = "UTF-8";
+
+    try {
+      DummyChunkSampleStream predictedSample = new DummyChunkSampleStream(
+          new PlainTextByLineStream(
+              new InputStreamReader(inPredicted, encoding)), true);
+
+      DummyChunkSampleStream expectedSample = new DummyChunkSampleStream(
+          new PlainTextByLineStream(new InputStreamReader(inExpected)), false);
+
+      Chunker dummyChunker = new DummyChunker(predictedSample);
+
+      EvaluationSampleListener<ChunkSample> listener = new ChunkerDetailedFMeasureListener();
+      ChunkerEvaluator evaluator = new ChunkerEvaluator(dummyChunker,
+          Collections.singletonList(listener));
+
+      evaluator.evaluate(expectedSample);
+
+      StringBuilder expected = new StringBuilder();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(detailedOutputStream, encoding));
+      String line = reader.readLine();
+      
+      while(line != null ) {
+        expected.append(line);
+        expected.append("\n");
+        line = reader.readLine();
+      }
+      assertEquals(expected.toString().trim(), listener.toString().trim());
+    } finally {
+      inPredicted.close();
+      inExpected.close();
+      detailedOutputStream.close();
+    }
+  }
+}
