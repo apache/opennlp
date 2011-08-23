1a1a312caa0b3b54e4a604dc141ebc962b5edb1c
William Daniel Colen de Moura Silva <colen@apache.org>
2011-08-23 14:53:33 +0000

    OPENNLP-226 TokenNameFinder Monitors are passed as variable length param.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1160725 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/cmdline/namefind/NameEvaluationErrorListener.java
+++ opennlp-tools/src/main/java/opennlp/tools/cmdline/namefind/NameEvaluationErrorListener.java
@@ -21,6 +21,7 @@ import java.io.OutputStream;
 
 import opennlp.tools.cmdline.EvaluationErrorPrinter;
 import opennlp.tools.namefind.NameSample;
+import opennlp.tools.namefind.TokenNameFinderEvaluationMonitor;
 import opennlp.tools.util.eval.EvaluationMonitor;
 
 /**
@@ -29,7 +30,7 @@ import opennlp.tools.util.eval.EvaluationMonitor;
  * 
  */
 public class NameEvaluationErrorListener extends
-    EvaluationErrorPrinter<NameSample> {
+    EvaluationErrorPrinter<NameSample> implements TokenNameFinderEvaluationMonitor {
 
   /**
    * Creates a listener that will print to System.err
