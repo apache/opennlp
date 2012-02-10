e0840b1f6dada34cb078118e04ddc88fdc7aac96
William Daniel Colen de Moura Silva <colen@apache.org>
2012-02-10 03:33:56 +0000

    OPENNLP-429: Created a default POS Sequence Validator outside the POSTaggerME (latter will delete the one inside it) and created an initial version of the Factory.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/opennlp/trunk@1242647 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/main/java/opennlp/tools/postag/DefaultPOSSequenceValidator.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreemnets.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.postag;
+
+import java.util.Arrays;
+
+import opennlp.tools.util.SequenceValidator;
+
+public class DefaultPOSSequenceValidator implements SequenceValidator<String> {
+
+  private POSDictionary tagDictionary;
+
+  public DefaultPOSSequenceValidator(POSDictionary tagDictionary) {
+    this.tagDictionary = tagDictionary;
+  }
+
+  public boolean validSequence(int i, String[] inputSequence,
+      String[] outcomesSequence, String outcome) {
+    if (tagDictionary == null) {
+      return true;
+    } else {
+      String[] tags = tagDictionary.getTags(inputSequence[i].toString());
+      if (tags == null) {
+        return true;
+      } else {
+        return Arrays.asList(tags).contains(outcome);
+      }
+    }
+  }
+}
