3abe3b7e2b593811902c34edfbfa66eb48aed376
William Daniel Colen de Moura Silva <colen@apache.org>
2012-03-23 02:51:40 +0000

    OPENNLP-481: ADTokenSampleStream now uses a customized DictionaryDetokenizer that handles hyphens
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1304169 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/test/java/opennlp/tools/formats/ad/ADTokenSampleStreamTest.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.formats.ad;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import opennlp.tools.tokenize.TokenSample;
+import opennlp.tools.util.ObjectStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ADTokenSampleStreamTest {
+
+  List<TokenSample> samples = new ArrayList<TokenSample>();
+
+  @Test
+  public void testSimpleCount() throws IOException {
+    assertEquals(6, samples.size()); // means that there are 3 documents
+  }
+
+  @Test
+  public void testSentences() throws IOException {
+    assertTrue(samples.get(5).getText().contains("ofereceu-me"));
+  }
+
+  @Before
+  public void setup() throws IOException, URISyntaxException {
+    ADTokenSampleStreamFactory factory = new ADTokenSampleStreamFactory(
+        ADTokenSampleStreamFactory.Parameters.class);
+
+    File dict = new File(getClass().getClassLoader()
+        .getResource("opennlp/tools/tokenize/latin-detokenizer.xml").toURI());
+    File data = new File(getClass().getClassLoader()
+        .getResource("opennlp/tools/formats/ad.sample").toURI());
+    String[] args = { "-data", data.getCanonicalPath(), "-encoding", "UTF-8",
+        "-lang", "pt", "-detokenizer", dict.getCanonicalPath() };
+    ObjectStream<TokenSample> tokenSampleStream = factory.create(args);
+
+    TokenSample sample = tokenSampleStream.read();
+
+    while (sample != null) {
+      samples.add(sample);
+      sample = tokenSampleStream.read();
+    }
+
+  }
+
+}
