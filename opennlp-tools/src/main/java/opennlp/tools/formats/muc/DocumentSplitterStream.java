3de7694d4aac9953152901e35d5e2cc617f9db66
Jörn Kottmann <joern@apache.org>
2012-03-28 17:58:23 +0000

    OPENNLP-341 Implemented a stream to split MUC files into document strings.
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1306518 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/main/java/opennlp/tools/formats/muc/DocumentSplitterStream.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.formats.muc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import opennlp.tools.util.FilterObjectStream;
+import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.ObjectStream;
+
+class DocumentSplitterStream extends FilterObjectStream<String, String> {
+  
+  private static final String DOC_START_ELEMENT = "<DOC>";
+  private static final String DOC_END_ELEMENT = "</DOC>";
+  
+  private List<String> docs = new ArrayList<String>();
+  
+  DocumentSplitterStream(ObjectStream<String> samples) {
+    super(samples);
+  }
+
+  public String read() throws IOException {
+    
+    if (docs.isEmpty()) {
+      String newDocs = samples.read();
+      
+      if (newDocs != null) {
+        int docStartOffset = 0;
+        
+        while (true) {
+          int startDocElement = newDocs.indexOf(DOC_START_ELEMENT, docStartOffset);
+          int endDocElement = newDocs.indexOf(DOC_END_ELEMENT, docStartOffset);
+          
+          if (startDocElement != -1 && endDocElement != -1) {
+            
+            if (startDocElement < endDocElement) {
+              docs.add(newDocs.substring(startDocElement, endDocElement + DOC_END_ELEMENT.length()));
+              docStartOffset = endDocElement + DOC_END_ELEMENT.length();
+            }
+            else {
+              throw new InvalidFormatException("<DOC> element is not closed!");
+            }
+          }
+          else if (startDocElement != endDocElement) {
+            throw new InvalidFormatException("Missing <DOC> or </DOC> element!");            
+          }
+          else {
+            break;
+          }
+        }
+      }
+    }
+    
+    if (docs.size() > 0) {
+      return docs.remove(0);
+    }
+    else {
+      return null;
+    }
+  }
+}
---
+++ opennlp-tools/src/test/java/opennlp/tools/formats/muc/DocumentSplitterStreamTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.formats.muc;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.ObjectStreamUtils;
+
+public class DocumentSplitterStreamTest {
+
+  @Test
+  public void testSplitTwoDocuments() throws IOException {
+    
+    StringBuilder docsString = new StringBuilder();
+
+    for (int i = 0; i < 2; i++) {
+      docsString.append("<DOC>\n");
+      docsString.append("test document #"+ i + "\n");
+      docsString.append("</DOC>\n");
+    }
+    
+    ObjectStream<String> docs = new DocumentSplitterStream(
+        ObjectStreamUtils.createObjectStream(docsString.toString()));
+    
+    String doc1 = docs.read();
+    Assert.assertEquals(docsString.length() / 2, doc1.length() + 1);
+    Assert.assertTrue(doc1.contains("#0"));
+    
+    String doc2 = docs.read();
+    Assert.assertEquals(docsString.length() / 2, doc2.length() + 1);
+    Assert.assertTrue(doc2.contains("#1"));
+    
+    Assert.assertNull(docs.read());
+    Assert.assertNull(docs.read());
+  }
+}
