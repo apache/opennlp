f568b61a4490664f7b390de90ef575fca8a8637f
Jörn Kottmann <joern@apache.org>
2012-04-03 07:50:44 +0000

    OPENNLP-341 Simple test for the sgml parser.
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1308735 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/test/java/opennlp/tools/formats/muc/SgmlParserTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.formats.muc;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.junit.Test;
+
+public class SgmlParserTest {
+
+  @Test
+  public void testParse1() throws IOException {
+
+    Reader in = new InputStreamReader(SgmlParserTest.class.getResourceAsStream("parsertest1.sgml"), "UTF-8");
+    
+    try {
+      SgmlParser parser = new SgmlParser();
+      parser.parse(in, new SgmlParser.ContentHandler() {
+      });
+    }
+    finally {
+      in.close();
+    }
+    
+  }
+  
+}
