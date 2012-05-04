5fa3b90323bb0755904118548ca1cc25ed7dad23
Jörn Kottmann <joern@apache.org>
2012-05-04 14:13:17 +0000

    OPENNLP-342 New Parse Sample converters
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1333973 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/main/java/opennlp/tools/formats/convert/ParseToSentenceSampleStreamFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.formats.convert;
+
+import opennlp.tools.cmdline.ArgumentParser;
+import opennlp.tools.cmdline.StreamFactoryRegistry;
+import opennlp.tools.cmdline.params.DetokenizerParameter;
+import opennlp.tools.formats.DetokenizerSampleStreamFactory;
+import opennlp.tools.formats.ParseSampleStreamFactory;
+import opennlp.tools.parser.Parse;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.sentdetect.SentenceSample;
+import opennlp.tools.util.ObjectStream;
+
+/**
+ * <b>Note:</b> Do not use this class, internal use only!
+ */
+public class ParseToSentenceSampleStreamFactory extends DetokenizerSampleStreamFactory<SentenceSample> {
+
+  interface Parameters extends ParseSampleStreamFactory.Parameters, DetokenizerParameter {
+  }
+  
+  private ParseToSentenceSampleStreamFactory() {
+    super(Parameters.class);
+  }
+
+  public ObjectStream<SentenceSample> create(String[] args) {
+    Parameters params = ArgumentParser.parse(args, Parameters.class);
+    language = params.getLang();
+    
+    ObjectStream<Parse> parseSampleStream = StreamFactoryRegistry.getFactory(Parse.class,
+        StreamFactoryRegistry.DEFAULT_FORMAT).create(
+        ArgumentParser.filter(args, Parameters.class));
+    
+    return new POSToSentenceSampleStream(createDetokenizer(params),
+        new ParseToPOSSampleStream(parseSampleStream), 30);
+  }
+  
+  public static void registerFactory() {
+    StreamFactoryRegistry.registerFactory(SentenceSample.class,
+        "parse", new ParseToSentenceSampleStreamFactory());
+  }
+}
