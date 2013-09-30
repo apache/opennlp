c6392185d398af5aaa63b83aad9be7daf9090464
Jörn Kottmann <joern@apache.org>
2013-09-30 14:44:18 +0000

    OPENNLP-582 Added lemmatizer component with implementation based on the dictionary classes. Thanks to Rodrigo Agerri for the contribution.
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1527600 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/main/java/opennlp/tools/lemmatizer/DictionaryLemmatizer.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.lemmatizer;
+
+public interface DictionaryLemmatizer {
+  
+  /**
+   * Returns the lemma of the specified word with the specified part-of-speech.
+   * 
+   * @param word The word whose lemmas are desired.
+   * @param pos The part-of-speech of the specified word.
+   * @return The lemma of the specified word given the specified part-of-speech.
+   */
+  public String lemmatize(String word, String postag);
+
+
+}
