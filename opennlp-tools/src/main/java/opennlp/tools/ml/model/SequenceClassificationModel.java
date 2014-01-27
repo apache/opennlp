23c64058bcd2e295a757d213300cc3ff771416d5
Jörn Kottmann <joern@apache.org>
2014-01-27 11:30:52 +0000

    OPENNLP-641 Added interface for sequence classification model
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1561639 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/main/java/opennlp/tools/ml/model/SequenceClassificationModel.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package opennlp.tools.ml.model;
+
+import opennlp.tools.util.BeamSearchContextGenerator;
+import opennlp.tools.util.SequenceValidator;
+import opennlp.tools.util.Sequence;
+
+/**
+ * A classification model that can label an input sequence.
+ *
+ * @param <T>
+ */
+public interface SequenceClassificationModel<T> {
+
+  /**
+   * Finds the sequence with the highest probability.
+   * 
+   * @param sequence
+   * @param additionalContext
+   * @param cg
+   * @param validator
+   * 
+   * @return
+   */
+  Sequence bestSequence(T[] sequence, Object[] additionalContext,
+      BeamSearchContextGenerator<T> cg, SequenceValidator<T> validator);
+  
+  /**
+   * Finds the n most probable sequences.
+   * 
+   * @param sequence
+   * @param additionalContext
+   * @param cg
+   * @param validator
+   * 
+   * @return
+   */
+  Sequence[] bestSequences(int numSequences, T[] sequence,
+      Object[] additionalContext, BeamSearchContextGenerator<T> cg, SequenceValidator<T> validator);
+}
