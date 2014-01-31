53958bb2d159f5b95cebcc32f2f64a415a03e3dd
Mark Giaconia <markg@apache.org>
2014-01-31 13:04:24 +0000

    OPENNLP-643 Initial rough cut. Modified the RegexNameFinder to process a list of regexes by type via a constructor with a map. Added a RegexNameFinderFactory which can return a set of defaults as a RegexNameFinder. May have been overly creative with the Enum...All the regexes still need work, but they are effective for the most basic cases. Need non US P# regexes
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1563130 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/namefind/RegexNameFinder.java
+++ opennlp-tools/src/main/java/opennlp/tools/namefind/RegexNameFinder.java
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package opennlp.tools.namefind;
 
 import java.util.Collection;
@@ -32,8 +30,17 @@ import opennlp.tools.util.Span;
  */
 public final class RegexNameFinder implements TokenNameFinder {
 
-  private final Pattern mPatterns[];
-  private final String sType;
+  private Pattern mPatterns[];
+  private String sType;
+  private Map<String, Pattern[]> regexMap;
+
+  public RegexNameFinder(Map<String, Pattern[]> regexMap) {
+    if (regexMap == null) {
+      throw new IllegalArgumentException("regexNameFinders must not be null");
+    }
+    this.regexMap = regexMap;
+
+  }
 
   public RegexNameFinder(Pattern patterns[], String type) {
     if (patterns == null || patterns.length == 0) {
@@ -52,11 +59,12 @@ public final class RegexNameFinder implements TokenNameFinder {
     mPatterns = patterns;
     sType = null;
   }
-  
-  public Span[] find(String tokens[]) {
-    Map<Integer, Integer> sentencePosTokenMap = new HashMap<Integer, Integer>();
 
-    StringBuffer sentenceString = new StringBuffer(tokens.length *  10);
+  @Override
+  public Span[] find(String tokens[]) {
+    Map<Integer, Integer> sentencePosTokenMap = new HashMap<>();
+
+    StringBuffer sentenceString = new StringBuffer(tokens.length * 10);
 
     for (int i = 0; i < tokens.length; i++) {
 
@@ -73,29 +81,112 @@ public final class RegexNameFinder implements TokenNameFinder {
       }
     }
 
-    Collection<Span> annotations = new LinkedList<Span>();
+    Collection<Span> annotations = new LinkedList<>();
 
-    for (Pattern mPattern : mPatterns) {
-      Matcher matcher = mPattern.matcher(sentenceString);
+    if (mPatterns == null && regexMap != null) {
+      for (Map.Entry<String, Pattern[]> entry : regexMap.entrySet()) {
+        for (Pattern mPattern : entry.getValue()) {
+          Matcher matcher = mPattern.matcher(sentenceString);
 
-      while (matcher.find()) {
-        Integer tokenStartIndex =
-            sentencePosTokenMap.get(matcher.start());
-        Integer tokenEndIndex =
-            sentencePosTokenMap.get(matcher.end());
+          while (matcher.find()) {
+            Integer tokenStartIndex =
+                    sentencePosTokenMap.get(matcher.start());
+            Integer tokenEndIndex =
+                    sentencePosTokenMap.get(matcher.end());
 
-        if (tokenStartIndex != null && tokenEndIndex != null) {
+            if (tokenStartIndex != null && tokenEndIndex != null) {
+              Span annotation = new Span(tokenStartIndex, tokenEndIndex, entry.getKey());
+              annotations.add(annotation);
+            }
+          }
+        }
+      }
+    } else {
+      for (Pattern mPattern : mPatterns) {
+        Matcher matcher = mPattern.matcher(sentenceString);
+
+        while (matcher.find()) {
+          Integer tokenStartIndex =
+                  sentencePosTokenMap.get(matcher.start());
+          Integer tokenEndIndex =
+                  sentencePosTokenMap.get(matcher.end());
+
+          if (tokenStartIndex != null && tokenEndIndex != null) {
+            Span annotation = new Span(tokenStartIndex, tokenEndIndex, sType);
+            annotations.add(annotation);
+          }
+        }
+      }
+    }
+
+
+    return annotations.toArray(
+            new Span[annotations.size()]);
+  }
+
+  /**
+   * NEW. This method removes the need for tokenization, but returns the Span
+   * with character indices, rather than word.
+   *
+   * @param text
+   * @return
+   */
+  public Span[] find(String text) {
+    return getAnnotations(text);
+  }
+
+  private Span[] getAnnotations(String text) {
+    Collection<Span> annotations = new LinkedList<>();
+    if (mPatterns == null && regexMap != null) {
+      for (Map.Entry<String, Pattern[]> entry : regexMap.entrySet()) {
+        for (Pattern mPattern : entry.getValue()) {
+          Matcher matcher = mPattern.matcher(text);
+
+          while (matcher.find()) {
+            Integer tokenStartIndex = matcher.start();
+            Integer tokenEndIndex = matcher.end();
+            Span annotation = new Span(tokenStartIndex, tokenEndIndex, entry.getKey());
+            annotations.add(annotation);
+
+          }
+        }
+      }
+    } else {
+      for (Pattern mPattern : mPatterns) {
+        Matcher matcher = mPattern.matcher(text);
+
+        while (matcher.find()) {
+          Integer tokenStartIndex = matcher.start();
+          Integer tokenEndIndex = matcher.end();
           Span annotation = new Span(tokenStartIndex, tokenEndIndex, sType);
           annotations.add(annotation);
+
         }
       }
     }
 
     return annotations.toArray(
-        new Span[annotations.size()]);
+            new Span[annotations.size()]);
   }
-  
+
+  @Override
   public void clearAdaptiveData() {
     // nothing to clear
+  }
+
+  public Pattern[] getmPatterns() {
+    return mPatterns;
+  }
+
+  public void setmPatterns(Pattern[] mPatterns) {
+    this.mPatterns = mPatterns;
+  }
+
+  public String getsType() {
+    return sType;
+  }
+
+  public void setsType(String sType) {
+    this.sType = sType;
   }
 }
