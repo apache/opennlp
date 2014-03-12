528033b3762fbafc191b6a5b35d17f37207346ce
Jörn Kottmann <joern@apache.org>
2014-03-12 11:12:38 +0000

    OPENNLP-605 Added a method to extract serializers from a custom feature generator configuration.
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1576692 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/test/java/opennlp/tools/util/featuregen/FeatureGenWithSerializerMapping.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.util.featuregen;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import opennlp.tools.util.model.SerializableArtifact;
+
+public class FeatureGenWithSerializerMapping implements AdaptiveFeatureGenerator, ArtifactToSerializerMapper {
+
+  @Override
+  public void createFeatures(List<String> features, String[] tokens, int index,
+      String[] previousOutcomes) {
+  }
+
+  @Override
+  public void updateAdaptiveData(String[] tokens, String[] outcomes) {
+  }
+
+  @Override
+  public void clearAdaptiveData() {
+  }
+
+  @Override
+  public Map<String, Class<? extends SerializableArtifact>> getArtifactSerializerMapping() {
+    Map<String, Class<? extends SerializableArtifact>> mapping = new HashMap<>();
+    mapping.put("test.resource", W2VClassesDictionary.class);
+    return Collections.unmodifiableMap(mapping);
+  }
+}
