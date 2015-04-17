8111cafdc6428a8af8115982f5f98378df2c3334
Jorn Kottmann <joern@apache.org>
2015-04-17 13:16:55 +0000

    OPENNLP-768 Added cross validation support for the parser.
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1674298 13f79535-47bb-0310-9956-ffa450edef68

---
+++ opennlp-tools/src/main/java/opennlp/tools/parser/ParserCrossEvaluator.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package opennlp.tools.parser;
+
+import java.io.IOException;
+
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.TrainingParameters;
+import opennlp.tools.util.eval.CrossValidationPartitioner;
+import opennlp.tools.util.eval.FMeasure;
+
+public class ParserCrossEvaluator {
+
+  private final String languageCode;
+
+  private final TrainingParameters params;
+
+  private final HeadRules rules;
+
+  private final FMeasure fmeasure = new FMeasure();
+
+  private ParserType parserType;
+
+  private ParserEvaluationMonitor[] monitors;
+
+  ParserCrossEvaluator(String languageCode, TrainingParameters params, HeadRules rules, ParserType parserType,
+      ParserEvaluationMonitor... monitors) {
+    this.languageCode = languageCode;
+    this.params = params;
+    this.rules = rules;
+    this.parserType = parserType;
+  }
+
+  public void evaluate(ObjectStream<Parse> samples, int nFolds) throws IOException {
+
+    CrossValidationPartitioner<Parse> partitioner = new CrossValidationPartitioner<Parse>(
+        samples, nFolds);
+
+    while (partitioner.hasNext()) {
+      CrossValidationPartitioner.TrainingSampleStream<Parse> trainingSampleStream = partitioner
+          .next();
+
+      ParserModel model;
+
+      if (ParserType.CHUNKING.equals(parserType)) {
+        model = opennlp.tools.parser.chunking.Parser.train(languageCode, samples, rules, params);
+      }
+      else if (ParserType.TREEINSERT.equals(parserType)) {
+        model = opennlp.tools.parser.treeinsert.Parser.train(languageCode, samples, rules, params);
+      }
+      else {
+        throw new IllegalStateException("Unexpected parser type: " + parserType);
+      }
+
+      ParserEvaluator evaluator = new ParserEvaluator(ParserFactory.create(model), monitors);
+
+      evaluator.evaluate(trainingSampleStream.getTestSampleStream());
+
+      fmeasure.mergeInto(evaluator.getFMeasure());
+    }
+  }
+
+  public FMeasure getFMeasure() {
+    return fmeasure;
+  }
+}
