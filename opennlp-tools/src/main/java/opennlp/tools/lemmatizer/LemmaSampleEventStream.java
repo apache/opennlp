411d0612e7f8b38967c173011e26655cf28cb293
Rodrigo Agerri <ragerri@apache.org>
2016-04-27 13:59:04 +0000

    OPENNLP-760 adding APL header to lemmatizer component classes
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1741267 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/lemmatizer/LemmaSampleEventStream.java
+++ opennlp-tools/src/main/java/opennlp/tools/lemmatizer/LemmaSampleEventStream.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package opennlp.tools.lemmatizer;
 
 import java.util.ArrayList;
@@ -32,9 +48,9 @@ public class LemmaSampleEventStream extends AbstractEventStream<LemmaSample> {
       List<Event> events = new ArrayList<Event>();
       String[] toksArray = sample.getTokens();
       String[] tagsArray = sample.getTags();
-      String[] predsArray = sample.getLemmas();
+      String[] lemmasArray = sample.getLemmas();
       for (int ei = 0, el = sample.getTokens().length; ei < el; ei++) {
-        events.add(new Event(predsArray[ei], contextGenerator.getContext(ei,toksArray,tagsArray,predsArray)));
+        events.add(new Event(lemmasArray[ei], contextGenerator.getContext(ei,toksArray,tagsArray,lemmasArray)));
       }
       return events.iterator();
     }
