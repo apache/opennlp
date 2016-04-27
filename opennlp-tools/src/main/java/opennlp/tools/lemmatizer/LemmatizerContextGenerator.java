c2df440580c168cbde585ef8d7e66ed446b81c56
Rodrigo Agerri <ragerri@apache.org>
2016-04-27 13:59:04 +0000

    OPENNLP-760 adding APL header to lemmatizer component classes
    git-svn-id: https://svn.apache.org/repos/asf/opennlp/trunk@1741267 13f79535-47bb-0310-9956-ffa450edef68

--- opennlp-tools/src/main/java/opennlp/tools/lemmatizer/LemmatizerContextGenerator.java
+++ opennlp-tools/src/main/java/opennlp/tools/lemmatizer/LemmatizerContextGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package opennlp.tools.lemmatizer;
 
 import opennlp.tools.util.BeamSearchContextGenerator;
@@ -12,9 +28,9 @@ public interface LemmatizerContextGenerator extends BeamSearchContextGenerator<S
    * @param i The index of the token in the specified toks array for which the context should be constructed.
    * @param toks The tokens of the sentence.  The <code>toString</code> methods of these objects should return the token text.
    * @param tags The POS tags for the the specified tokens.
-   * @param preds The previous decisions made in the tagging of this sequence.  Only indices less than i will be examined.
+   * @param lemmas The previous decisions made in the tagging of this sequence.  Only indices less than i will be examined.
    * @return An array of predictive contexts on which a model basis its decisions.
    */
-  public String[] getContext(int i, String[] toks, String[] tags, String[] preds);
+  public String[] getContext(int i, String[] toks, String[] tags, String[] lemmas);
 }
 
