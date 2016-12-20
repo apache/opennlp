4e9a977c1b792a30fd042a365313b8d6e893ac64
smarthi <smarthi@apache.org>
2016-12-19 23:25:10 -0500

    OPENNLP-894: Switch from Java 7 to Java 8 and enforce Maven 3.3.9

--- opennlp-uima/src/test/java/opennlp/uima/AnnotatorsInitializationTest.java
+++ opennlp-uima/src/test/java/opennlp/uima/AnnotatorsInitializationTest.java
@@ -16,6 +16,8 @@
  */
 package opennlp.uima;
 
+import java.io.File;
+import java.io.IOException;
 import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.cas.CAS;
@@ -24,10 +26,6 @@ import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.ResourceSpecifier;
 import org.apache.uima.util.InvalidXMLException;
 import org.apache.uima.util.XMLInputSource;
-import org.junit.Test;
-
-import java.io.File;
-import java.io.IOException;
 
 import static org.junit.Assert.fail;
 
@@ -38,7 +36,9 @@ public class AnnotatorsInitializationTest {
 
   private static final String PATHNAME = "src/test/resources/test-descriptors/";
 
-  @Test
+  // TODO: This test requires the SourceForge models, or other models to run,
+  // but they are missing due to license issues since the project was migrated to Apache
+  //@Test
   public void testInitializationExecutionAndReconfigure() {
     File f = new File(PATHNAME);
     for (String descName : f.list(new FileUtil.ExtFilenameFilter("xml"))) {
