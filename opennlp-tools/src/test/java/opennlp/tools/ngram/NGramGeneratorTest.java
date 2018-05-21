25cfc14c330217b2d270df5cbda58adf84f727cb
koji <koji@apache.org>
2018-05-21 12:29:23 +0900

    OPENNLP-1198: add more tests to NGramGeneratorTest

--- opennlp-tools/src/test/java/opennlp/tools/ngram/NGramGeneratorTest.java
+++ opennlp-tools/src/test/java/opennlp/tools/ngram/NGramGeneratorTest.java
@@ -25,43 +25,151 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class NGramGeneratorTest {
-  
+
   @Test
-  public void generateListTest() {
-    
+  public void generateListTest1() {
+
+    final List<String> input = Arrays.asList("This", "is", "a", "sentence");
+    final int window = 1;
+    final String separator = "-";
+
+    final List<String> ngrams = NGramGenerator.generate(input, window, separator);
+
+    Assert.assertEquals(4,  ngrams.size());
+    Assert.assertEquals("This", ngrams.get(0));
+    Assert.assertEquals("is", ngrams.get(1));
+    Assert.assertEquals("a", ngrams.get(2));
+    Assert.assertEquals("sentence", ngrams.get(3));
+
+  }
+
+  @Test
+  public void generateListTest2() {
+
     final List<String> input = Arrays.asList("This", "is", "a", "sentence");
     final int window = 2;
     final String separator = "-";
-    
+
     final List<String> ngrams = NGramGenerator.generate(input, window, separator);
-    
+
     Assert.assertEquals(3,  ngrams.size());
-    Assert.assertTrue(ngrams.contains("This-is"));
-    Assert.assertTrue(ngrams.contains("is-a"));
-    Assert.assertTrue(ngrams.contains("a-sentence"));
-    
+    Assert.assertEquals("This-is", ngrams.get(0));
+    Assert.assertEquals("is-a", ngrams.get(1));
+    Assert.assertEquals("a-sentence", ngrams.get(2));
+
   }
-  
+
+  @Test
+  public void generateListTest3() {
+
+    final List<String> input = Arrays.asList("This", "is", "a", "sentence");
+    final int window = 3;
+    final String separator = "-";
+
+    final List<String> ngrams = NGramGenerator.generate(input, window, separator);
+
+    Assert.assertEquals(2, ngrams.size());
+    Assert.assertEquals("This-is-a", ngrams.get(0));
+    Assert.assertEquals("is-a-sentence", ngrams.get(1));
+
+  }
+
+  @Test
+  public void generateListTest4() {
+
+    final List<String> input = Arrays.asList("This", "is", "a", "sentence");
+    final int window = 4;
+    final String separator = "-";
+
+    final List<String> ngrams = NGramGenerator.generate(input, window, separator);
+
+    Assert.assertEquals(1, ngrams.size());
+    Assert.assertEquals("This-is-a-sentence", ngrams.get(0));
+
+  }
+
+  @Test
+  public void generateCharTest1() {
+
+    final char[] input = "Test".toCharArray();
+    final int window = 1;
+    final String separator = "-";
+
+    final List<String> ngrams = NGramGenerator.generate(input, window, separator);
+
+    Assert.assertEquals(4, ngrams.size());
+    Assert.assertEquals("T", ngrams.get(0));
+    Assert.assertEquals("e", ngrams.get(1));
+    Assert.assertEquals("s", ngrams.get(2));
+    Assert.assertEquals("t", ngrams.get(3));
+
+  }
+
+  @Test
+  public void generateCharTest2() {
+
+    final char[] input = "Test".toCharArray();
+    final int window = 2;
+    final String separator = "-";
+
+    final List<String> ngrams = NGramGenerator.generate(input, window, separator);
+
+    Assert.assertEquals(3,  ngrams.size());
+    Assert.assertEquals("T-e", ngrams.get(0));
+    Assert.assertEquals("e-s", ngrams.get(1));
+    Assert.assertEquals("s-t", ngrams.get(2));
+
+  }
+
+  @Test
+  public void generateCharTest3() {
+
+    final char[] input = "Test".toCharArray();
+    final int window = 3;
+    final String separator = "-";
+
+    final List<String> ngrams = NGramGenerator.generate(input, window, separator);
+
+    Assert.assertEquals(2,  ngrams.size());
+    Assert.assertEquals("T-e-s", ngrams.get(0));
+    Assert.assertEquals("e-s-t", ngrams.get(1));
+
+  }
+
+  @Test
+  public void generateCharTest4() {
+
+    final char[] input = "Test".toCharArray();
+    final int window = 4;
+    final String separator = "-";
+
+    final List<String> ngrams = NGramGenerator.generate(input, window, separator);
+
+    Assert.assertEquals(1,  ngrams.size());
+    Assert.assertEquals("T-e-s-t", ngrams.get(0));
+
+  }
+
   @Test
   public void generateCharTest() {
-    
+
     final char[] input = "Test again".toCharArray();
     final int window = 4;
     final String separator = "-";
-    
+
     final List<String> ngrams = NGramGenerator.generate(input, window, separator);
 
     Assert.assertEquals(7,  ngrams.size());
-    Assert.assertTrue(ngrams.contains("T-e-s-t"));
-    Assert.assertTrue(ngrams.contains("e-s-t- "));
-    Assert.assertTrue(ngrams.contains("s-t- -a"));
-    Assert.assertTrue(ngrams.contains("t- -a-g"));
-    Assert.assertTrue(ngrams.contains(" -a-g-a"));
-    Assert.assertTrue(ngrams.contains("a-g-a-i"));
-    Assert.assertTrue(ngrams.contains("g-a-i-n"));
-    
+    Assert.assertEquals(("T-e-s-t"), ngrams.get(0));
+    Assert.assertEquals(("e-s-t- "), ngrams.get(1));
+    Assert.assertEquals(("s-t- -a"), ngrams.get(2));
+    Assert.assertEquals(("t- -a-g"), ngrams.get(3));
+    Assert.assertEquals((" -a-g-a"), ngrams.get(4));
+    Assert.assertEquals(("a-g-a-i"), ngrams.get(5));
+    Assert.assertEquals(("g-a-i-n"), ngrams.get(6));
+
   }
-  
+
   @Test
   public void generateLargerWindowThanListTest() {
     
