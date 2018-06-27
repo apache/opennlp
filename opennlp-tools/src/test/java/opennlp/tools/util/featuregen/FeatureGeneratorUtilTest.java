8414c1e46cfa4aea6f20e0795b7fb487f72c0ac9
Koji Sekiguchi <koji.sekiguchi@rondhuit.com>
2018-06-27 22:25:02 +0900

    OPENNLP-1197: fix bug, recognizing symbols (including #, %. & etc) (#322)

--- opennlp-tools/src/test/java/opennlp/tools/util/featuregen/FeatureGeneratorUtilTest.java
+++ opennlp-tools/src/test/java/opennlp/tools/util/featuregen/FeatureGeneratorUtilTest.java
@@ -47,6 +47,9 @@ public class FeatureGeneratorUtilTest {
     Assert.assertEquals("other", FeatureGeneratorUtil.tokenFeature("."));
     Assert.assertEquals("other", FeatureGeneratorUtil.tokenFeature("?"));
     Assert.assertEquals("other", FeatureGeneratorUtil.tokenFeature("!"));
+    Assert.assertEquals("other", FeatureGeneratorUtil.tokenFeature("#"));
+    Assert.assertEquals("other", FeatureGeneratorUtil.tokenFeature("%"));
+    Assert.assertEquals("other", FeatureGeneratorUtil.tokenFeature("&"));
   }
 
   @Test
