c55665cc598a80fbaf98892c7664013de90728df
jzonthemtn <jzemerick@apache.org>
2022-01-19 08:57:59 -0500

    OPENNLP-1353: Fixing checkstyle issues.

--- opennlp-tools/src/main/java/opennlp/tools/lemmatizer/DictionaryLemmatizer.java
+++ opennlp-tools/src/main/java/opennlp/tools/lemmatizer/DictionaryLemmatizer.java
@@ -17,11 +17,20 @@
 
 package opennlp.tools.lemmatizer;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Lemmatize by simple dictionary lookup into a hashmap built from a file
