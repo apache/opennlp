a78d4a2846347ab110099cc36aa1dae1a85c78c2
jzonthemtn <jeff.zemerick@mtnfog.com>
2022-04-20 10:02:06 -0400

    OPENNLP-1368: Checking for version 1 model.

--- opennlp-tools/src/main/java/opennlp/tools/util/model/BaseModel.java
+++ opennlp-tools/src/main/java/opennlp/tools/util/model/BaseModel.java
@@ -416,6 +416,12 @@ public abstract class BaseModel implements ArtifactProvider, Serializable {
 
       // Version check is only performed if current version is not the dev/debug version
       if (!Version.currentVersion().equals(Version.DEV_VERSION)) {
+        // Support OpenNLP 1.x models.
+        if (version.getMajor() != 1) {
+          throw new InvalidFormatException("Model version " + version + " is not supported by this ("
+              + Version.currentVersion() + ") version of OpenNLP!");
+        }
+
         // Reject loading a snapshot model with a non-snapshot version
         if (!Version.currentVersion().isSnapshot() && version.isSnapshot()) {
           throw new InvalidFormatException("Model version " + version
