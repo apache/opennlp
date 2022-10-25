eeefaaf1d5d8b65b59e7a45d0d409b7c27a5ce86
Martin Wiesner <martin.wiesner@hs-heilbronn.de>
2022-10-25 08:24:16 +0200

    OPENNLP-1366: Fix Training of MaxEnt Model with large corpora fails with java.io.UTFDataFormatException

--- opennlp-tools/src/main/java/opennlp/tools/ml/perceptron/BinaryPerceptronModelWriter.java
+++ opennlp-tools/src/main/java/opennlp/tools/ml/perceptron/BinaryPerceptronModelWriter.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.util.zip.GZIPOutputStream;
 
 import opennlp.tools.ml.model.AbstractModel;
+import opennlp.tools.ml.model.ModelParameterChunker;
 
 /**
  * Model writer that saves models in binary format.
@@ -65,7 +66,7 @@ public class BinaryPerceptronModelWriter extends PerceptronModelWriter {
   }
 
   public void writeUTF(String s) throws java.io.IOException {
-    output.writeUTF(s);
+    ModelParameterChunker.writeUTF(output, s);
   }
 
   public void writeInt(int i) throws java.io.IOException {
