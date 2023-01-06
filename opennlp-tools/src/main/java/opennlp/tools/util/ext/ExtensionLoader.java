933dedf9a679a92593965eef790d8ad131b55023
atita arora <atitaarora@gmail.com>
2023-01-06 16:05:57 +0100

    OPENNLP-1135 : Removed support for OSGi

--- opennlp-tools/src/main/java/opennlp/tools/util/ext/ExtensionLoader.java
+++ opennlp-tools/src/main/java/opennlp/tools/util/ext/ExtensionLoader.java
@@ -26,17 +26,7 @@ import java.lang.reflect.Field;
  */
 public class ExtensionLoader {
 
-  private static boolean isOsgiAvailable = false;
-
   private ExtensionLoader() {
-  }
-
-  static boolean isOSGiAvailable() {
-    return isOsgiAvailable;
-  }
-
-  static void setOSGiAvailable() {
-    isOsgiAvailable = true;
   }
 
   // Pass in the type (interface) of the class to load
@@ -94,25 +84,6 @@ public class ExtensionLoader {
       }
     } catch (ClassNotFoundException e) {
       // Class is not on classpath
-    }
-
-    // Loading from class path failed
-
-    // Either something is wrong with the class name or OpenNLP is
-    // running in an OSGi environment. The extension classes are not
-    // on our classpath in this case.
-    // In OSGi we need to use services to get access to extensions.
-
-    // Determine if OSGi class is on class path
-
-    // Now load class which depends on OSGi API
-    if (isOsgiAvailable) {
-
-      // The OSGIExtensionLoader class will be loaded when the next line
-      // is executed, but not prior, and that is why it is safe to directly
-      // reference it here.
-      OSGiExtensionLoader extLoader = OSGiExtensionLoader.getInstance();
-      return extLoader.getExtension(clazz, extensionClassName);
     }
 
     throw new ExtensionNotLoadedException("Unable to find implementation for " +
