23ac5bd5dde46c1cae2fe83b3a8d6b4c2ef3b348
Lara Marinov <lmarinov@andrew.cmu.edu>
2023-12-09 11:54:58 -0500

    Remove integration test per comment from @mawiesne

--- opennlp-tools/src/test/java/opennlp/tools/tokenize/TokenizerMEIT.java
+++ opennlp-tools/src/test/java/opennlp/tools/tokenize/TokenizerMEIT.java
@@ -36,16 +36,4 @@ public class TokenizerMEIT {
     Assertions.assertEquals(",", tokens[1]);
   }
 
-  @Test
-  void testTokenizerDownloadedModelDe() throws IOException {
-
-    TokenizerME tokenizer = new TokenizerME("de");
-
-    String[] tokens = tokenizer.tokenize("prüfen,");
-
-    Assertions.assertEquals(2, tokens.length);
-    Assertions.assertEquals("prüfen", tokens[0]);
-    Assertions.assertEquals(",", tokens[1]);
-  }
-
 }
