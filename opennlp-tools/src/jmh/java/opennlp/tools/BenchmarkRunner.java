c038db1a572a2b2b43fdf463f08364ca381a6137
Richard Zowalla <rzo1@apache.org>
2023-12-14 20:59:11 +0100

    OPENNLP-421 - Add GC Profiler

--- opennlp-tools/src/jmh/java/opennlp/tools/BenchmarkRunner.java
+++ opennlp-tools/src/jmh/java/opennlp/tools/BenchmarkRunner.java
@@ -16,10 +16,21 @@
  */
 package opennlp.tools;
 
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 public class BenchmarkRunner {
 
   public static void main(String[] args) throws Exception {
-    org.openjdk.jmh.Main.main(args);
+    Options opt = new OptionsBuilder()
+            .include(StringListBenchmark.class.getSimpleName())
+//            .addProfiler(StackProfiler.class)
+            .addProfiler(GCProfiler.class)
+            .build();
+
+    new Runner(opt).run();
   }
 
 }
