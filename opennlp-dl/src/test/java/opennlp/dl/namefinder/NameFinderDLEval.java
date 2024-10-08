1923b0b58297c0799644446e72e0a60fe07b08a3
Richard Zowalla <rzo1@apache.org>
2024-10-08 11:26:58 +0200

    OPENNLP-1618 - AbstractDL does not release Ort Resources

--- opennlp-dl/src/test/java/opennlp/dl/namefinder/NameFinderDLEval.java
+++ opennlp-dl/src/test/java/opennlp/dl/namefinder/NameFinderDLEval.java
@@ -36,7 +36,7 @@ import opennlp.tools.util.Span;
 public class NameFinderDLEval extends AbstractDLTest {
 
   private static final Logger logger = LoggerFactory.getLogger(NameFinderDLEval.class);
-  private final SentenceDetector sentenceDetector ;
+  private final SentenceDetector sentenceDetector;
 
   public NameFinderDLEval() throws IOException {
     this.sentenceDetector = new SentenceDetectorME("en");
@@ -54,18 +54,21 @@ public class NameFinderDLEval extends AbstractDLTest {
     final String[] tokens = new String[]
         {"George", "Washington", "was", "president", "of", "the", "United", "States", "."};
 
-    final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(), sentenceDetector);
-    final Span[] spans = nameFinderDL.find(tokens);
+    try (final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(),
+        sentenceDetector)) {
+      final Span[] spans = nameFinderDL.find(tokens);
 
-    for (Span span : spans) {
-      logger.debug(span.toString());
+      for (Span span : spans) {
+        logger.debug(span.toString());
+      }
+
+      Assertions.assertEquals(1, spans.length);
+      Assertions.assertEquals(0, spans[0].getStart());
+      Assertions.assertEquals(17, spans[0].getEnd());
+      Assertions.assertEquals(8.251646041870117, spans[0].getProb(), 0.00001);
+      Assertions.assertEquals("George Washington",
+          spans[0].getCoveredText(String.join(" ", tokens)));
     }
-
-    Assertions.assertEquals(1, spans.length);
-    Assertions.assertEquals(0, spans[0].getStart());
-    Assertions.assertEquals(17, spans[0].getEnd());
-    Assertions.assertEquals(8.251646041870117, spans[0].getProb(), 0.00001);
-    Assertions.assertEquals("George Washington", spans[0].getCoveredText(String.join(" ", tokens)));
 
   }
 
@@ -78,19 +81,20 @@ public class NameFinderDLEval extends AbstractDLTest {
     final File model = new File(getOpennlpDataDir(), "onnx/namefinder/model.onnx");
     final File vocab = new File(getOpennlpDataDir(), "onnx/namefinder/vocab.txt");
 
-    final String[] tokens = new String[]{"His", "name", "was", "George", "Washington"};
+    final String[] tokens = new String[] {"His", "name", "was", "George", "Washington"};
 
-    final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(), sentenceDetector);
-    final Span[] spans = nameFinderDL.find(tokens);
+    try (final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(),
+        sentenceDetector)) {
+      final Span[] spans = nameFinderDL.find(tokens);
 
-    for (Span span : spans) {
-      logger.debug(span.toString());
+      for (Span span : spans) {
+        logger.debug(span.toString());
+      }
+
+      Assertions.assertEquals(1, spans.length);
+      Assertions.assertEquals(13, spans[0].getStart());
+      Assertions.assertEquals(30, spans[0].getEnd());
     }
-
-    Assertions.assertEquals(1, spans.length);
-    Assertions.assertEquals(13, spans[0].getStart());
-    Assertions.assertEquals(30, spans[0].getEnd());
-
   }
 
   @Test
@@ -102,19 +106,20 @@ public class NameFinderDLEval extends AbstractDLTest {
     final File model = new File(getOpennlpDataDir(), "onnx/namefinder/model.onnx");
     final File vocab = new File(getOpennlpDataDir(), "onnx/namefinder/vocab.txt");
 
-    final String[] tokens = new String[]{"His", "name", "was", "George"};
+    final String[] tokens = new String[] {"His", "name", "was", "George"};
 
-    final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(), sentenceDetector);
-    final Span[] spans = nameFinderDL.find(tokens);
+    try (final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(),
+        sentenceDetector)) {
+      final Span[] spans = nameFinderDL.find(tokens);
 
-    for (Span span : spans) {
-      logger.debug(span.toString());
+      for (Span span : spans) {
+        logger.debug(span.toString());
+      }
+
+      Assertions.assertEquals(1, spans.length);
+      Assertions.assertEquals(13, spans[0].getStart());
+      Assertions.assertEquals(19, spans[0].getEnd());
     }
-
-    Assertions.assertEquals(1, spans.length);
-    Assertions.assertEquals(13, spans[0].getStart());
-    Assertions.assertEquals(19, spans[0].getEnd());
-
   }
 
   @Test
@@ -126,13 +131,14 @@ public class NameFinderDLEval extends AbstractDLTest {
     final File model = new File(getOpennlpDataDir(), "onnx/namefinder/model.onnx");
     final File vocab = new File(getOpennlpDataDir(), "onnx/namefinder/vocab.txt");
 
-    final String[] tokens = new String[]{};
+    final String[] tokens = new String[] {};
 
-    final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(), sentenceDetector);
-    final Span[] spans = nameFinderDL.find(tokens);
+    try (final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(),
+        sentenceDetector)) {
+      final Span[] spans = nameFinderDL.find(tokens);
 
-    Assertions.assertEquals(0, spans.length);
-
+      Assertions.assertEquals(0, spans.length);
+    }
   }
 
   @Test
@@ -144,12 +150,14 @@ public class NameFinderDLEval extends AbstractDLTest {
     final File model = new File(getOpennlpDataDir(), "onnx/namefinder/model.onnx");
     final File vocab = new File(getOpennlpDataDir(), "onnx/namefinder/vocab.txt");
 
-    final String[] tokens = new String[]{"I", "went", "to", "the", "park"};
+    final String[] tokens = new String[] {"I", "went", "to", "the", "park"};
 
-    final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(), sentenceDetector);
-    final Span[] spans = nameFinderDL.find(tokens);
+    try (final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(),
+        sentenceDetector)) {
+      final Span[] spans = nameFinderDL.find(tokens);
 
-    Assertions.assertEquals(0, spans.length);
+      Assertions.assertEquals(0, spans.length);
+    }
 
   }
 
@@ -162,21 +170,24 @@ public class NameFinderDLEval extends AbstractDLTest {
     final File model = new File(getOpennlpDataDir(), "onnx/namefinder/model.onnx");
     final File vocab = new File(getOpennlpDataDir(), "onnx/namefinder/vocab.txt");
 
-    final String[] tokens = new String[]{"George", "Washington", "and", "Abraham", "Lincoln",
+    final String[] tokens = new String[] {"George", "Washington", "and", "Abraham", "Lincoln",
         "were", "presidents"};
 
-    final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(), sentenceDetector);
-    final Span[] spans = nameFinderDL.find(tokens);
+    try (final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(),
+        sentenceDetector)) {
+      final Span[] spans = nameFinderDL.find(tokens);
 
-    for (Span span : spans) {
-      logger.debug(span.toString());
+      for (Span span : spans) {
+        logger.debug(span.toString());
+      }
+
+      Assertions.assertEquals(2, spans.length);
+      Assertions.assertEquals(0, spans[0].getStart());
+      Assertions.assertEquals(17, spans[0].getEnd());
+      Assertions.assertEquals(22, spans[1].getStart());
+      Assertions.assertEquals(37, spans[1].getEnd());
+
     }
-
-    Assertions.assertEquals(2, spans.length);
-    Assertions.assertEquals(0, spans[0].getStart());
-    Assertions.assertEquals(17, spans[0].getEnd());
-    Assertions.assertEquals(22, spans[1].getStart());
-    Assertions.assertEquals(37, spans[1].getEnd());
 
   }
 
@@ -190,7 +201,9 @@ public class NameFinderDLEval extends AbstractDLTest {
       final File model = new File("invalid.onnx");
       final File vocab = new File("vocab.txt");
 
-      new NameFinderDL(model, vocab, getIds2Labels(), sentenceDetector);
+      try (final NameFinderDL nameFinderDL = new NameFinderDL(model, vocab, getIds2Labels(),
+          sentenceDetector)) {
+      }
     });
 
   }
